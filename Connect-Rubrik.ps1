d01ba8a068031d58910563801a6c83489ee3e753
Chris Wahl <github@wahlnetwork.com>
2015-07-08 16:16:18 -0700

    Create test file and add NSX code to main file
    Signed-off-by: Chris Wahl <github@wahlnetwork.com>

--- Connect-Rubrik.ps1
+++ Connect-Rubrik.ps1
@@ -1,3 +1,66 @@
-﻿$uri = 'https://192.168.2.10:443/'
+﻿#Requires -Version 4.0
+
+<#  
+.SYNOPSIS  Creates a virtual network tier for VMware NSX
+.DESCRIPTION Creates a virtual network tier for VMware NSX
+.NOTES  Author:  Chris Wahl, @ChrisWahl, WahlNetwork.com
+.PARAMETER NSX
+	NSX Manager IP or FQDN
+.PARAMETER NSXPassword
+	NSX Manager credentials with administrative authority
+.PARAMETER NSXUsername
+	NSX Manager username with administrative authority
+.PARAMETER JSONPath
+	Path to your JSON configuration file
+.PARAMETER vCenter
+	vCenter Server IP or FQDN
+.PARAMETER NoAskCreds
+	Use your current login credentials for vCenter
+.EXAMPLE
+	PS> Create-NSXTier -NSX nsxmgr.tld -vCenter vcenter.tld -JSONPath "c:\path\prod.json"
+#>
+
+[CmdletBinding()]
+Param(
+	[Parameter(Mandatory=$true,Position=0,HelpMessage="NSX Manager IP or FQDN")]
+	[ValidateNotNullorEmpty()]
+	[String]$NSX,
+	[Parameter(Mandatory=$true,Position=1,HelpMessage="NSX Manager credentials with administrative authority")]
+	[ValidateNotNullorEmpty()]
+	[System.Security.SecureString]$NSXPassword,
+	[Parameter(Mandatory=$true,Position=2,HelpMessage="Path to your JSON configuration file")]
+	[ValidateNotNullorEmpty()]
+	[String]$JSONPath,
+	[Parameter(Mandatory=$true,Position=3,HelpMessage="vCenter Server IP or FQDN")]
+	[ValidateNotNullorEmpty()]
+	[String]$vCenter,
+	[String]$NSXUsername = "admin",
+	[Parameter(HelpMessage="Use your current login credentials for vCenter")]
+	[Switch]$NoAskCreds
+	)
+
+
+# Create NSX authorization string and store in $head
+$nsxcreds = New-Object System.Management.Automation.PSCredential "admin",$NSXPassword
+$auth = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($NSXUsername+":"+$($nsxcreds.GetNetworkCredential().password)))
+$head = @{"Authorization"="Basic $auth"}
+$uri = "https://$nsx"
+
+
+# Allow untrusted SSL certs
+	add-type @"
+	    using System.Net;
+	    using System.Security.Cryptography.X509Certificates;
+	    public class TrustAllCertsPolicy : ICertificatePolicy {
+	        public bool CheckValidationResult(
+	            ServicePoint srvPoint, X509Certificate certificate,
+	            WebRequest request, int certificateProblem) {
+	            return true;
+	        }
+	    }
+"@
+	[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
+$uri = 'https://192.168.2.10:443/'
 
 Invoke-WebRequest -Uri $uri
---
+++ Connect-Rubrik_test.ps1
@@ -0,0 +1,26 @@
+﻿# Allow untrusted SSL certs
+	add-type @"
+	    using System.Net;
+	    using System.Security.Cryptography.X509Certificates;
+	    public class TrustAllCertsPolicy : ICertificatePolicy {
+	        public bool CheckValidationResult(
+	            ServicePoint srvPoint, X509Certificate certificate,
+	            WebRequest request, int certificateProblem) {
+	            return true;
+	        }
+	    }
+"@
+	[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
+$uri = 'https://192.168.2.10:443/'
+
+$login_call = @{
+  userId = "admin"
+  password = "admin"
+}
+
+$r = Invoke-WebRequest -Uri ($uri + "login") -Method: Post -Body (ConvertTo-Json $login_call)
+
+$r = Invoke-WebRequest -Uri ($uri + "vm/list") -Method: Get -Headers @{"Authorization"="Basic OWU2MDMyMGMtYzFhYy00ZjkyLTgzZDAtYzk0NGU2ZjlkNjZhOg=="}
+
+$r.Content | ConvertFrom-Json
