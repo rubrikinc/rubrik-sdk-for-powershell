b9147d2fbc3f714dd38c201882916c31cc8f3fd3
Chris Wahl <github@wahlnetwork.com>
2015-08-15 11:28:39 -0500

    Create help information for Connect-Rubrik
    Signed-off-by: Chris Wahl <github@wahlnetwork.com>

--- Rubrik/Connect-Rubrik.ps1
+++ Rubrik/Connect-Rubrik.ps1
@@ -2,9 +2,18 @@
 function Connect-Rubrik 
 {
     <#  
-            .SYNOPSIS  Connects to Rubrik and retrieves a token value for authentication
-            .DESCRIPTION Connects to Rubrik and retrieves a token value for authentication
-            .NOTES  Author:  Chris Wahl, chris.wahl@rubrik.com
+            .SYNOPSIS
+            Connects to Rubrik and retrieves a token value for authentication
+            .DESCRIPTION
+            The Connect-Rubrik function is used to connect to the Rubrik RESTful API and supply
+            credentials to the /login method. Rubrik then returns a unique token to represent
+            the user's credentials for subsequent calls. Acquire a token before running other Rubrik cmdlets.
+            .NOTES
+            Written by Chris Wahl for community usage
+            Twitter: @ChrisWahl
+            Email: Chris.Wahl@Rubrik.com
+            .LINK
+            https://github.com/rubrikinc/PowerShell-Module
             .PARAMETER Username
             The Rubrik username
             .PARAMETER Password
@@ -12,7 +21,11 @@ function Connect-Rubrik
             .PARAMETER Server
             The Rubrik FQDN or IP address
             .EXAMPLE
-            PS> tbd
+            Connect-Rubrik -Username admin -Password secret -Server 192.168.1.1
+            This will connect to Rubrik with a username of "admin" and a password of "secret" to the IP address 192.168.1.1.
+            .EXAMPLE
+            Connect-Rubrik admin secret 192.168.1.1
+            Same as the previous example, except the arguments are supplied silently and in a strict order.
     #>
 
     [CmdletBinding()]
