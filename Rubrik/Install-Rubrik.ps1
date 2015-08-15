de382a196998a4e798eb889f91fddaaddaeca54f
Chris Wahl <github@wahlnetwork.com>
2015-08-15 11:52:38 -0500

    Create Rubrik module installer
    Signed-off-by: Chris Wahl <github@wahlnetwork.com>

---
+++ Rubrik/Install-Rubrik.ps1
@@ -0,0 +1,37 @@
+﻿#requires -Version 1
+$InstallPath = "$Home\Documents\WindowsPowerShell\Modules\Rubrik"
+
+$title = 'Rubrik Module Installer'
+$message = "Installation Path: $InstallPath`r`nAre you sure you wish to install or update your Rubrik module?"
+$yes = New-Object -TypeName System.Management.Automation.Host.ChoiceDescription -ArgumentList '&Yes', 'Yes'
+
+$no = New-Object -TypeName System.Management.Automation.Host.ChoiceDescription -ArgumentList '&No', 'No'
+
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+$result = $host.ui.PromptForChoice($title, $message, $options, 0)
+
+switch ($result)
+{
+    0 
+    {
+        If (Test-Path -Path $InstallPath)
+        {
+            Write-Host -Object 'Found previous installation of Rubrik module. Updating ...'
+            Remove-Item -Path $InstallPath -Recurse -Force
+            New-Item -ItemType Directory -Path $InstallPath
+        }
+        Else 
+        {
+            Write-Host -Object 'No previous installation of Rubrik module found. Installing ...'
+            New-Item -ItemType Directory -Path $InstallPath
+        }
+
+        Copy-Item $PSScriptRoot\* $InstallPath -Force -Recurse
+    }
+
+
+    1 
+    {
+        Write-Host -Object 'Installation aborted'
+    }
+}
