4d53bc3204955c8bb7f3fae711d7c4b6f42eed5e
Chris Wahl <github@wahlnetwork.com>
2016-03-28 15:25:11 -0500

    Configure AppVeyor YAML
    Signed-off-by: Chris Wahl <github@wahlnetwork.com>

--- Tests/Connect-Rubrik.Tests.ps1
+++ Tests/Connect-Rubrik.Tests.ps1
@@ -1,16 +1,24 @@
 ﻿# Initial Setup
 Import-Module -Name Rubrik -Force
-[string]$cluster = Get-Content -Path $PSScriptRoot'\TestVars\test-ip.txt'
-$cred = Import-Clixml -Path $PSScriptRoot'\TestVars\test-cred.xml'
 
-Describe "Connectivity Tests" {
-    It "Attempting to ping the Rubrik Test Cluster" {
-        $ping = Test-Connection -ComputerName $cluster.Split(":")[0] -Quiet
+# Variables
+if ((Test-Path -Path $PSScriptRoot'\TestVars\test-ip.txt') -eq $true) 
+{
+    [string]$global:cluster = Get-Content -Path $PSScriptRoot'\TestVars\test-ip.txt'
+}
+if ((Test-Path -Path $PSScriptRoot'\TestVars\test-cred.xml') -eq $true) 
+{
+    $global:cred = Import-Clixml -Path $PSScriptRoot'\TestVars\test-cred.xml'
+}
+
+Describe -Name 'Connectivity Tests' -Fixture {
+    It -name 'Attempting to ping the Rubrik Test Cluster' -test {
+        $ping = Test-Connection -ComputerName $global:cluster.Split(':')[0] -Quiet
         $ping | Should be $true
     }
 
-    It "Connects to a Rubrik Test Cluster and gathers a token" {
-        Connect-Rubrik -Server $cluster -Credential $cred
+    It -name 'Connects to a Rubrik Test Cluster and gathers a token' -test {
+        Connect-Rubrik -Server $global:cluster -Credential $global:cred
         $($global:RubrikConnection.token) | Should Be $true
     }
 }
