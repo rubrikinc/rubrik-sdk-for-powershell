59524dbcdcf21c10cc74cd8f203ccd796860fb2a
Chris Wahl <github@wahlnetwork.com>
2016-04-28 10:55:22 -0500

    Update cmdlets with helper functions
    Signed-off-by: Chris Wahl <github@wahlnetwork.com>

--- Rubrik/Public/New-RubrikSnapshot.ps1
+++ Rubrik/Public/New-RubrikSnapshot.ps1
@@ -5,64 +5,52 @@ function New-RubrikSnapshot
             .SYNOPSIS
             Takes a Rubrik snapshot of a virtual machine
             .DESCRIPTION
-            The New-RubrikSnapshot cmdlet will trigger an on-demand snapshot for a specific virtual machine. This will be taken by
-            Rubrik and stored in the VM's chain of snapshots.
+            The New-RubrikSnapshot cmdlet will trigger an on-demand snapshot for a specific virtual machine. This will be taken by Rubrik and stored in the VM's chain of snapshots.
             .NOTES
             Written by Chris Wahl for community usage
             Twitter: @ChrisWahl
             GitHub: chriswahl
             .LINK
             https://github.com/rubrikinc/PowerShell-Module
+            .EXAMPLE
+            New-RubrikSnapshot -VM 'Server1'
+            This will trigger an on-demand backup for the virtual machine named Server1
     #>
 
     [CmdletBinding()]
     Param(
-        [Parameter(Mandatory = $true,Position = 0,HelpMessage = 'Virtual Machine to backup',ValueFromPipeline=$true)]
+        [Parameter(Mandatory = $true,Position = 0,HelpMessage = 'Virtual Machine to backup',ValueFromPipeline = $true)]
         [Alias('Name')]
         [ValidateNotNullorEmpty()]
         [String]$VM,
         [Parameter(Mandatory = $false,Position = 1,HelpMessage = 'Rubrik FQDN or IP address')]
         [ValidateNotNullorEmpty()]
-        [String]$Server = $global:RubrikServer
+        [String]$Server = $global:RubrikConnection.server
     )
 
     Process {
 
-        # Validate the Rubrik token exists
-        if (-not $global:RubrikToken) 
-        {
-            throw 'You are not connected to a Rubrik server. Use Connect-Rubrik.'
-        }
+        TestRubrikConnection
 
-        # Query Rubrik for the VM ID
-        $uri = 'https://'+$global:RubrikServer+':443/vm'
-        try 
-        {
-            $r = Invoke-WebRequest -Uri $uri -Headers $global:RubrikHead -Method Get
-            $result = (ConvertFrom-Json -InputObject $r.Content) | Where-Object {$_.name -eq $VM}
-            if (!$result) {throw 'No VM found with that name.'}
-        }
-        catch 
-        {
-            throw 'Error connecting to Rubrik server'
-        }
+        Write-Verbose -Message 'Gathering VM ID value from Rubrik'
+        $vmid = (Get-RubrikVM -VM $VM).id
 
-        # Submit the request for an On-Demand Backup
-        $uri = 'https://'+$global:RubrikServer+':443/internal/job/type/backup'
+        Write-Verbose -Message 'Submit the request for an On-Demand Backup'
+        $uri = 'https://'+$Server+'/internal/job/type/backup'
 
         $body = @{
-          vmId = $result.id
-          isOnDemandSnapshot = "true"
+            vmId               = $vmid
+            isOnDemandSnapshot = 'true'
         }
 
         try 
         {
-            $r = Invoke-WebRequest -Uri $uri -Headers $global:RubrikHead -Method Post -Body (ConvertTo-Json -InputObject $body)
-            Write-Host "A new on-demand backup job has been created for $($result.name).`r`nThe job ID is as follows:`r`n$($r.Content)"
+            $r = Invoke-WebRequest -Uri $uri -Headers $Header -Method Post -Body (ConvertTo-Json -InputObject $body)
+            Write-Verbose -Message $r.Content
         }
         catch 
         {
-            throw 'Error connecting to Rubrik server'
+            throw $_
         }
 
 
