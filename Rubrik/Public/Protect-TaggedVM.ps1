17d289b200e01e1ced74f136bbbef7f7593e56e6
jasonburrell <jason.burrell@rubrik.com>
2016-05-16 14:24:12 -0700

    Allows someone to use tags from vSphere to assign VMs to a Rubrik SLA
    Added Protect-TaggedVM

---
+++ Rubrik/Public/Protect-TaggedVM.ps1
@@ -0,0 +1,98 @@
+﻿#Requires -Version 3
+#Requires -modules VMware.VimAutomation.Core
+
+function Protect-TaggedVM
+{
+    <#
+            .SYNOPSIS
+            Connects to Rubrik and assigns an SLA based on a tag value in vSphere
+            .DESCRIPTION
+            The Protect-TaggedVM cmdlet will update a virtual machine's SLA Domain assignment within the Rubrik cluster. The SLA Domain contains all policy-driven values needed to protect workloads.
+            .NOTES
+            Written by Jason Burrell for community usage
+            Twitter: @jasonburrell2
+            .LINK
+            https://github.com/rubrikinc/PowerShell-Module
+            .EXAMPLE
+            Protect-TaggedVM -vSphereTag 'Gold' -SLA 'Gold'
+            This will assign the Gold SLA Domain to a VM tagged Gold
+            Protect-TaggedVM -vSphereTag 'Gold' -Unprotect
+            This will remove the SLA Domain assigned to tagged VMs, thus rendering them unprotected
+    #>
+
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true,Position = 0,HelpMessage = 'vSphere Tag',ValueFromPipeline = $true)]
+        [ValidateNotNullorEmpty()]
+        [String]$Tag,
+        [Parameter(Mandatory = $true,Position = 1,HelpMessage = 'The SLA Domain in Rubrik',ValueFromPipeline = $true)]
+        [ValidateNotNullorEmpty()]
+        [String]$SLA,
+        [Parameter(Mandatory = $false,Position = 2,HelpMessage = 'Removes the SLA Domain assignment',ValueFromPipeline = $true)]
+        [ValidateNotNullorEmpty()]
+        [Switch]$DoNotProtect,
+        [Parameter(Mandatory = $false,Position = 3,HelpMessage = 'Rubrik FQDN or IP address')]
+        [ValidateNotNullorEmpty()]
+        [String]$Server = $global:RubrikConnection.server
+    )
+
+     Process {
+
+       TestRubrikConnection
+
+        Write-Verbose -Message 'Matching the SLA input to a valid Rubrik SLA Domain'
+        try
+        {
+            if ($DoNotProtect)
+            {
+                $SLAmatch = @{}
+                $SLAmatch.id = 'UNPROTECTED'
+                $SLAmatch.name = 'Unprotected'
+            }
+            else
+            {
+                $SLAmatch = Get-RubrikSLA -SLA $SLA
+            }
+            if ($SLAmatch -eq $null)
+            {
+                Write-Warning -Message "No matching SLA Domain found with the name `"$SLA`"`nThe following SLA Domains were found:"
+                Get-RubrikSLA | Select-Object -Property Name
+                break
+            }
+        }
+        catch
+        {
+            throw $_ 
+        }
+
+       
+        try
+        {
+        Write-Verbose -Message 'Gathering VMs tagged from vCenter Inventory Service'
+       
+        $vms = get-vm -tag $Tag
+        } 
+        catch
+        {
+            throw $_
+        }
+
+        Write-Verbose -Message 'VMs to be updated:'
+
+        Foreach ($VM in $VMs) {
+            Write-host  "Assign $VM to $SLA"
+        }
+
+        $Continue = Read-Host -Prompt 'Continue (Y/N)'
+        if ($Continue -eq 'Y') {
+        Foreach ($VM in $VMs) {
+            Write-Host "Protecting $VM"
+            Protect-RubrikVM -VM $VM.Name -SLA $SLA
+
+        }
+        } else {
+        Write-Verbose "Operation canceled"
+        }
+
+    } # End of process
+} # End of function
