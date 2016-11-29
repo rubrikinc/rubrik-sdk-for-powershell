337bf2ea44b34950160e67eb58d4c9efc63e5ca0
Chris Wahl <github@wahlnetwork.com>
2016-11-29 12:01:57 -0600

    Update Get-RubrikVM Tests with new hashtable name
    Signed-off-by: Chris Wahl <github@wahlnetwork.com>

--- Tests/Get-RubrikVM.Tests.ps1
+++ Tests/Get-RubrikVM.Tests.ps1
@@ -1,7 +1,7 @@
 ﻿# Import
 Import-Module -Name "$PSScriptRoot\..\Rubrik" -Force
 . "$(Split-Path -Parent -Path $PSScriptRoot)\Rubrik\Private\Get-RubrikAPIData.ps1"
-$resources = GetRubrikAPIData -endpoint ('VMwareVM')
+$resources = GetRubrikAPIData -endpoint ('VMwareVMGet')
 
 # Pester
 
