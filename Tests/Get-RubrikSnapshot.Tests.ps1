0d7a14fc8aefb41fbe3efdc4219fdc131da31624
Chris Wahl <github@wahlnetwork.com>
2016-12-03 13:55:27 -0600

    Create scafolding for new unit tests
    Signed-off-by: Chris Wahl <github@wahlnetwork.com>

---
+++ Tests/Get-RubrikSnapshot.Tests.ps1
@@ -0,0 +1,10 @@
+﻿# Import
+Import-Module -Name "$PSScriptRoot\..\Rubrik" -Force
+. "$(Split-Path -Parent -Path $PSScriptRoot)\Rubrik\Private\Get-RubrikAPIData.ps1"
+$resources = GetRubrikAPIData -endpoint ('VMwareVMSnapshotGet')
+
+# Pester
+
+Describe -Name 'Get-RubrikSnapshot Tests' -Fixture {
+  
+}
---
+++ Tests/New-RubrikMount.Tests.ps1
@@ -0,0 +1,10 @@
+﻿# Import
+Import-Module -Name "$PSScriptRoot\..\Rubrik" -Force
+. "$(Split-Path -Parent -Path $PSScriptRoot)\Rubrik\Private\Get-RubrikAPIData.ps1"
+$resources = GetRubrikAPIData -endpoint ('VMwareVMMountPost')
+
+# Pester
+
+Describe -Name 'New-RubrikMount Tests' -Fixture {
+  
+}
