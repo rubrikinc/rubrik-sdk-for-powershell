b1c1c05316d924b5194c4dd7e1c50fbe426abcf8
Chris Wahl <github@wahlnetwork.com>
2016-12-27 13:24:03 -0600

    Update New-RubrikSnapshot
    Signed-off-by: Chris Wahl <github@wahlnetwork.com>

--- Rubrik/Public/New-RubrikSnapshot.ps1
+++ Rubrik/Public/New-RubrikSnapshot.ps1
@@ -1,58 +1,92 @@
 #Requires -Version 3
 function New-RubrikSnapshot
 {
-    <#  
-            .SYNOPSIS
-            Takes a Rubrik snapshot of a virtual machine
-            .DESCRIPTION
-            The New-RubrikSnapshot cmdlet will trigger an on-demand snapshot for a specific virtual machine. This will be taken by Rubrik and stored in the VM's chain of snapshots.
-            .NOTES
-            Written by Chris Wahl for community usage
-            Twitter: @ChrisWahl
-            GitHub: chriswahl
-            .LINK
-            https://github.com/rubrikinc/PowerShell-Module
-            .EXAMPLE
-            New-RubrikSnapshot -VM 'Server1'
-            This will trigger an on-demand backup for the virtual machine named Server1
-    #>
+  <#  
+      .SYNOPSIS
+      Takes a Rubrik snapshot of a virtual machine
 
-    [CmdletBinding()]
-    Param(
-        [Parameter(Mandatory = $true,Position = 0,HelpMessage = 'Virtual Machine to backup',ValueFromPipeline = $true)]
-        [Alias('Name')]
-        [ValidateNotNullorEmpty()]
-        [String]$VM,
-        [Parameter(Mandatory = $false,Position = 1,HelpMessage = 'Rubrik FQDN or IP address')]
-        [ValidateNotNullorEmpty()]
-        [String]$Server = $global:RubrikConnection.server
-    )
+      .DESCRIPTION
+      The New-RubrikSnapshot cmdlet will trigger an on-demand snapshot for a specific virtual machine. This will be taken by Rubrik and stored in the VM's chain of snapshots.
 
-    Process {
+      .NOTES
+      Written by Chris Wahl for community usage
+      Twitter: @ChrisWahl
+      GitHub: chriswahl
 
-        TestRubrikConnection
+      .LINK
+      https://github.com/rubrikinc/PowerShell-Module
 
-        Write-Verbose -Message 'Gathering VM ID value from Rubrik'
-        $vmid = (Get-RubrikVM -VM $VM).id
+      .EXAMPLE
+      New-RubrikSnapshot -VM 'Server1'
+      This will trigger an on-demand backup for the virtual machine named Server1
+  #>
 
-        Write-Verbose -Message 'Submit the request for an On-Demand Backup'
-        $uri = 'https://'+$Server+'/job/type/backup'
+  [CmdletBinding(SupportsShouldProcess = $true,ConfirmImpact = 'High')]
+  Param(
+    # Virtual machine name
+    [Parameter(Mandatory = $true,Position = 0,ValueFromPipelineByPropertyName = $true)]
+    [Alias('Name')]
+    [ValidateNotNullorEmpty()]
+    [String]$VM,
+    # Rubrik server IP or FQDN
+    [Parameter(Position = 1)]
+    [String]$Server = $global:RubrikConnection.server,
+    # API version
+    [Parameter(Position = 2)]
+    [String]$api = $global:RubrikConnection.api
+  )
 
-        $body = @{
-            vmId               = $vmid
-            isOnDemandSnapshot = 'true'
-        }
+  Process {
 
-        try 
-        {
-            $r = Invoke-WebRequest -Uri $uri -Headers $Header -Method Post -Body (ConvertTo-Json -InputObject $body)
-            Write-Verbose -Message $r.Content
-        }
-        catch 
-        {
-            throw $_
-        }
+    TestRubrikConnection
+        
+    Write-Verbose -Message 'Determining which version of the API to use'
+    $resources = GetRubrikAPIData -endpoint ('VMwareVMBackupPost')
+
+    Write-Verbose -Message 'Gathering VM ID value from Rubrik'
+    $vmid = (Get-RubrikVM -VM $VM).id
+
+    Write-Verbose -Message 'Building the URI'
+    $uri = 'https://'+$Server+$resources.$api.URI
+    # Replace the placeholder of {id} with the actual VM ID
+    $uri = $uri -replace '{id}', $vmid
+
+    # v0 API Body is required for passing along the ID
+    if ($api -eq 'v0') 
+    {
+      Write-Verbose -Message 'Build the body'
+
+      $body = @{
+        vmId = $vmid
+      }
+    }
+    # v1+ API uses the VM ID in the URI
+    # The Invoke-WebRequest cmdlet will just overlook the body
+    else 
+    {
+      $body = $null
+    }
+
+    # Set the method
+    $method = $resources.$api.Method
+
+    Write-Verbose -Message 'Submit the request'
+    try
+    {
+      $r = Invoke-WebRequest -Uri $uri -Headers $Header -Method $method -Body (ConvertTo-Json -InputObject $body)
+      if ($r.StatusCode -ne $resources.$api.SuccessCode) 
+      {
+        Write-Warning -Message 'Did not receive successful status code from Rubrik'
+        throw $_
+      }
+      $response = ConvertFrom-Json -InputObject $r.Content
+      return $response
+    }
+    catch
+    {
+      throw $_
+    }
 
 
-    } # End of process
+  } # End of process
 } # End of function
---
+++ Tests/New-RubrikSnapshot.Tests.ps1
@@ -0,0 +1,39 @@
+﻿# Import
+Import-Module -Name "$PSScriptRoot\..\Rubrik" -Force
+. "$(Split-Path -Parent -Path $PSScriptRoot)\Rubrik\Private\Get-RubrikAPIData.ps1"
+$resources = GetRubrikAPIData -endpoint ('VMwareVMBackupPost')
+
+# Begin Pester tests
+Describe -Name 'New-RubrikSnapshot Tests' -Fixture {
+  # Test to make sure that resources were loaded
+  It -name 'Ensure that Resources are loaded' -test {
+    $resources | Should Not BeNullOrEmpty
+  }
+
+  # Set the API context and begin testing
+  foreach ($api in $resources.Keys)
+  {
+    if ($resources.$api.SuccessMock) 
+    {
+      Context -Name "set to API $api" -Fixture {
+        It -name 'Snapshot Request' -test {
+          # Arrange    
+          Mock -CommandName Invoke-WebRequest -Verifiable -MockWith {
+            return @{
+              Content    = $resources.$api.SuccessMock
+              StatusCode = $resources.$api.SuccessCode
+            }
+          } `
+          -ModuleName Rubrik
+    
+          # Act
+          $value1 = (ConvertFrom-Json -InputObject $resources.$api.SuccessMock).requestId
+          (New-RubrikSnapshot -VM TEST1 -api $api).requestId | Should BeExactly $value1           
+    
+          # Assert
+          Assert-VerifiableMocks
+        }
+      }
+    }
+  }
+} 
