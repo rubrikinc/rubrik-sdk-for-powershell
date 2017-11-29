b930b492929e5ff293d0c7a8d392d6cadfa61ff1
Troy Lindsay <tlindsay42@gmail.com>
2017-11-29 16:16:23 -0600

    Skip Unblock-SelfSignedCert if $PSVersionTable.Platform -eq Unix

--- Rubrik/Private/Unblock-SelfSignedCert.ps1
+++ Rubrik/Private/Unblock-SelfSignedCert.ps1
@@ -1,24 +1,31 @@
 ﻿<#
-    Helper function to allow self-signed certificates for HTTPS connections
-    This is required when using RESTful API calls over PowerShell
+  Helper function to allow self-signed certificates for HTTPS connections
+  This is required when using RESTful API calls over PowerShell
 #>
 function Unblock-SelfSignedCert() 
 {
   Write-Verbose -Message 'Allowing self-signed certificates'
-    
-  if ([System.Net.ServicePointManager]::CertificatePolicy -notlike 'TrustAllCertsPolicy') 
+
+  if ($PSVersionTable.Platform -eq 'Unix')
   {
-    Add-Type -TypeDefinition @"
-    using System.Net;
-    using System.Security.Cryptography.X509Certificates;
-    public class TrustAllCertsPolicy : ICertificatePolicy {
-	    public bool CheckValidationResult(
-	        ServicePoint srvPoint, X509Certificate certificate,
-	        WebRequest request, int certificateProblem) {
-	        return true;
-	    }
-    }
+    Write-Verbose -Message "Skipping $($MyInvocation.MyCommand.Name) when `$PSVersionTable.Platform = 'Unix'"
+  }
+  Else
+  {
+    if ([System.Net.ServicePointManager]::CertificatePolicy -notlike 'TrustAllCertsPolicy') 
+    {
+      Add-Type -TypeDefinition @"
+      using System.Net;
+      using System.Security.Cryptography.X509Certificates;
+      public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+          ServicePoint srvPoint, X509Certificate certificate,
+          WebRequest request, int certificateProblem) {
+          return true;
+        }
+      }
 "@
-    [System.Net.ServicePointManager]::CertificatePolicy = New-Object -TypeName TrustAllCertsPolicy
+      [System.Net.ServicePointManager]::CertificatePolicy = New-Object -TypeName TrustAllCertsPolicy
+    }
   }
 }
