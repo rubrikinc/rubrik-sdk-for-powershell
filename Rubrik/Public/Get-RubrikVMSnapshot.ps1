05d9d0c9834f40c60dc01ba5288baefb0cb8c3e0
Pierre Flammer <pierre.flammer@gmail.com>
2019-02-18 10:36:34 +0100

    pvf

---
+++ Rubrik/Public/Get-RubrikVMSnapshot.ps1
@@ -0,0 +1,64 @@
+﻿#requires -Version 3
+function Get-RubrikVMSnapshot
+{
+  <#  
+      .SYNOPSIS
+      Retrieves details on one or more virtual machines known to a Rubrik cluster
+
+      .DESCRIPTION
+      The Get-RubrikVMSnapshot cmdlet is used to pull a detailed information from a VM snapshot
+
+      .NOTES
+      Written by Pierre Flammer for community usage
+      Twitter: @PierreFlammer
+
+      .LINK
+      https://github.com/rubrikinc/PowerShell-Module
+
+      .EXAMPLE
+      Get-RubrikVMSnapshot -id 'cc1b363a-a0d4-40b7-9b09-7b8f3a805b27'
+      This will return details on the specific snapshot.
+  #>
+
+  [CmdletBinding()]
+  Param(
+    [Parameter(ValueFromPipelineByPropertyName = $true)]
+    [String]$id,
+    # Rubrik server IP or FQDN
+    [String]$Server = $global:RubrikConnection.server,
+    # API version
+    [String]$api = $global:RubrikConnection.api
+  )
+
+  Begin {
+
+    # The Begin section is used to perform one-time loads of data necessary to carry out the function's purpose
+    # If a command needs to be run with each iteration or pipeline input, place it in the Process section
+    
+    # Check to ensure that a session to the Rubrik cluster exists and load the needed header data for authentication
+    Test-RubrikConnection
+
+    # API data references the name of the function
+    # For convenience, that name is saved here to $function
+    $function = $MyInvocation.MyCommand.Name
+
+    # Retrieve all of the URI, method, body, query, result, filter, and success details for the API endpoint
+    Write-Verbose -Message "Gather API Data for $function"
+    $resources = Get-RubrikAPIData -endpoint $function
+    Write-Verbose -Message "Load API data for $($resources.Function)"
+    Write-Verbose -Message "Description: $($resources.Description)"
+  
+  }
+
+  Process {
+    $uri = New-URIString -server $Server -endpoint ($resources.URI) -id $id
+    $uri = Test-QueryParam -querykeys ($resources.Query.Keys) -parameters ((Get-Command $function).Parameters.Values) -uri $uri
+    $body = New-BodyString -bodykeys ($resources.Body.Keys) -parameters ((Get-Command $function).Parameters.Values)    
+    $result = Submit-Request -uri $uri -header $Header -method $($resources.Method) -body $body
+    $result = Test-ReturnFormat -api $api -result $result -location $resources.Result
+    $result = Test-FilterObject -filter ($resources.Filter) -result $result
+
+    return $result
+
+  } # End of process
+} # End of function
