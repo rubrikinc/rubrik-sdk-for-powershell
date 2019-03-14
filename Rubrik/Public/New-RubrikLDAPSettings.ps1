f386b8852ea4405340afe580071dec4e7284b5ac
Shuttleworth, Adam <ashuttleworth@irobot.com>
2019-03-13 22:03:17 -0400

    initial Commit - New function

---
+++ Rubrik/Public/New-RubrikLDAPSettings.ps1
@@ -0,0 +1,94 @@
+﻿#Requires -Version 3
+function New-RubrikLDAPSettings
+{
+  <#  
+      .SYNOPSIS
+      Connects to Rubrik and sets Rubrik cluster settings
+            
+      .DESCRIPTION
+      The New-RubrikSettings cmdlet will set the cluster settings on the system. This does require authentication.
+            
+      .NOTES
+      Adapted by Adam Shuttleworth from scripts by Chris Wahl for community usage
+            
+      .LINK
+      https://github.com/rubrikinc/PowerShell-Module
+            
+      .EXAMPLE
+      New-RubrikSettings -Name "Test LDAP Settings" -baseDN "DC=domain,DC=local" -authServers "192.168.1.8"
+      This will create LDAP settings on the Rubrik cluster defined by Connect-Rubrik function
+  #>
+
+  [CmdletBinding(SupportsShouldProcess = $true,ConfirmImpact = 'High')]
+  Param(
+    # Rubrik server IP or FQDN
+    [String]$Server = $global:RubrikConnection.server,
+    # ID of the Rubrik cluster or me for self
+    [String]$id = '',
+    # API version
+    [ValidateNotNullorEmpty()]
+    [String]$api = $global:RubrikConnection.api,
+    [Parameter(Mandatory=$True)]
+    [string]$name,
+    [Parameter(Mandatory=$True)]
+    [string]$dynamicDNSName,
+    [string]$baseDN,
+    [string[]]$authServers
+  )
+
+  Begin {
+
+    # The Begin section is used to perform one-time loads of data necessary to carry out the function's purpose
+    # If a command needs to be run with each iteration or pipeline input, place it in the Process section
+    $Credentials=$(Get-Credential -Message "Type AD Credentials.")
+    $bindUserName = $Credentials.UserName
+    $bindUserPassword = $Credentials.Password.ToString()
+    
+    #Write-Verbose "Username: $bindUserName"
+    #Write-Verbose "Password: $bindUserPassword"
+    # Check to ensure that a session to the Rubrik cluster exists and load the needed header data for authentication
+    Test-RubrikConnection
+    
+    # API data references the name of the function
+    # For convenience, that name is saved here to $function
+    $function = $MyInvocation.MyCommand.Name
+        
+    # Retrieve all of the URI, method, body, query, result, filter, and success details for the API endpoint
+    Write-Verbose -Message "Gather API Data for $function"
+    $resources = Get-RubrikAPIData -endpoint $function
+    Write-Verbose -Message "Load API data for $($resources.Function)"
+    Write-Verbose -Message "Description: $($resources.Description)"
+  
+  }
+
+  Process {
+    #region One-off
+    Write-Verbose -Message "Build the body"
+    #[PSCustomObject]$advancedOptions
+
+    #Write-Verbose "Advanced Options = $advancedOptions"
+
+    $body = @{
+        $resources.Body.dynamicDNSName = $dynamicDNSName
+        $resources.Body.bindUserName = $bindUserName
+        $resources.Body.bindUserPassword = $bindUserPassword
+        $resources.Body.baseDN = $baseDN
+        $resources.Body.name = $name
+        $resources.Body.authServers = $authServers
+    }
+        
+    $body = ConvertTo-Json $body
+    Write-Verbose -Message "Body = $body"
+    #endregion
+
+    $uri = New-URIString -server $Server -endpoint ($resources.URI) -id $id
+    $uri = Test-QueryParam -querykeys ($resources.Query.Keys) -parameters ((Get-Command $function).Parameters.Values) -uri $uri
+    #$body = New-BodyString -bodykeys ($resources.Body.Keys) -parameters ((Get-Command $function).Parameters.Values)
+    $result = Submit-Request -uri $uri -header $Header -method $($resources.Method) -body $body
+    $result = Test-ReturnFormat -api $api -result $result -location $resources.Result
+    $result = Test-FilterObject -filter ($resources.Filter) -result $result
+
+    return $result
+
+  } # End of process
+} # End of function
