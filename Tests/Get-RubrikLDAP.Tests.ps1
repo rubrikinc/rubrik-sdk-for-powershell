03a877ff363d028fe2e08a973ba6ea3e03bd73ea
Jaap Brasser <noreply@jaapbrasser.com>
2019-03-15 16:34:50 -0700

    Fixed path typo in LDAP tests

--- Tests/Get-RubrikLDAP.Tests.ps1
+++ Tests/Get-RubrikLDAP.Tests.ps1
@@ -1,7 +1,7 @@
 Remove-Module -Name 'Rubrik' -ErrorAction 'SilentlyContinue'
-Import-Module -Name '../Rubrik/Rubrik.psd1' -Force
+Import-Module -Name './Rubrik/Rubrik.psd1' -Force
 
-foreach ( $privateFunctionFilePath in ( Get-ChildItem -Path '../Rubrik/Private' | Where-Object extension -eq '.ps1').FullName  ) {
+foreach ( $privateFunctionFilePath in ( Get-ChildItem -Path './Rubrik/Private' | Where-Object extension -eq '.ps1').FullName  ) {
     . $privateFunctionFilePath
 }
 
