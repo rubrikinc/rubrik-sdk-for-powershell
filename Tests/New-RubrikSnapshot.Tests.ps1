91102b0ca77d06ad1d44ec5da4a9c982b438d1fa
Mike Preston <mwpreston@gmail.com>
2019-06-26 12:30:39 -0400

    added tests for New-RubrikSnapshot

---
+++ Tests/New-RubrikSnapshot.Tests.ps1
@@ -0,0 +1,58 @@
+Remove-Module -Name 'Rubrik' -ErrorAction 'SilentlyContinue'
+Import-Module -Name './Rubrik/Rubrik.psd1' -Force
+
+foreach ( $privateFunctionFilePath in ( Get-ChildItem -Path './Rubrik/Private' | Where-Object extension -eq '.ps1').FullName  ) {
+    . $privateFunctionFilePath
+}
+
+Describe -Name 'Public/New-RubrikSnapshot' -Tag 'Public', 'New-RubrikSnapshot' -Fixture {
+    #region init
+    $global:rubrikConnection = @{
+        id      = 'test-id'
+        userId  = 'test-userId'
+        token   = 'test-token'
+        server  = 'test-server'
+        header  = @{ 'Authorization' = 'Bearer test-authorization' }
+        time    = (Get-Date)
+        api     = 'v1'
+        version = '4.0.5'
+    }
+    #endregion
+
+    Context -Name 'Parameters' {
+        Mock -CommandName Test-RubrikConnection -Verifiable -ModuleName 'Rubrik' -MockWith {}
+        Mock -CommandName Test-RubrikSLA -Verifiable -ModuleName 'Rubrik' -MockWith {
+            @{
+                'slaid' = '11111'
+            }
+        }
+        Mock -CommandName Test-QueryParam -Verifiable -ModuleName 'Rubrik' -MockWith {
+            @{
+                'slaid' = 'https://server/api/internal/oracle/database/11111/snapshot'
+            }
+        }
+        Mock -CommandName Submit-Request -Verifiable -ModuleName 'Rubrik' -MockWith {
+            @{
+                'id'        = 'CREATE_ORACLE_SNAPSHOT_11111'
+                'status'    = 'QUEUED'
+                'progress'  = '0'
+            }
+        }
+        It -Name 'Should Return status of QUEUED' -Test {
+            ( New-RubrikSnapshot -id 1 -SLA 'Gold' -Confirm:$false).status |
+                Should -BeExactly 'QUEUED'
+        }
+        Context -Name 'Parameter Validation' {
+            It -Name 'Parameter id cannot be $null or empty' -Test {
+                { New-RubrikSnapshot -id $null } |
+                    Should -Throw "Cannot bind argument to parameter 'id' because it is an empty string."
+            }
+
+        }
+        Assert-VerifiableMock
+        Assert-MockCalled -CommandName Test-RubrikConnection -ModuleName 'Rubrik' -Times 1
+        Assert-MockCalled -CommandName Test-RubrikSLA -ModuleName 'Rubrik' -Times 1
+        Assert-MockCalled -CommandName Test-QueryParam -ModuleName 'Rubrik' -Times 1
+        Assert-MockCalled -CommandName Submit-Request -ModuleName 'Rubrik'  -Times 1
+    }
+}
