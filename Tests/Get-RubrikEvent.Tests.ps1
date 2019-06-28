4806ad0745658eace7186c38e7a17f826b8d33ac
Jaap Brasser <noreply@jaapbrasser.com>
2019-06-28 14:16:43 +0200

    Initial test for Get-RubrikEvent

---
+++ Tests/Get-RubrikEvent.Tests.ps1
@@ -0,0 +1,6 @@
+Remove-Module -Name 'Rubrik' -ErrorAction 'SilentlyContinue'
+Import-Module -Name './Rubrik/Rubrik.psd1' -Force
+
+foreach ( $privateFunctionFilePath in ( Get-ChildItem -Path './Rubrik/Private' | Where-Object extension -eq '.ps1').FullName  ) {
+    . $privateFunctionFilePath
+}
