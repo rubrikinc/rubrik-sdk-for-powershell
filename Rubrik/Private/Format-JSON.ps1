0ba17f92a175425fd611e5f8fac63696fdd54e88
Jaap Brasser <noreply@jaapbrasser.com>
2019-07-04 23:49:43 +0200

    Remove ParseJsonString, function was never called

--- Rubrik/Private/Format-JSON.ps1
+++ Rubrik/Private/Format-JSON.ps1
@@ -3,59 +3,61 @@
     http://stackoverflow.com/questions/16854057/convertfrom-json-max-length/27125027
 #>
 
-function ExpandPayload($response)
-{
+function ExpandPayload($response) {
+<#
+.SYNOPSIS
+This function use the .Net JSON Serializer in order to 
+#>
   [void][System.Reflection.Assembly]::LoadWithPartialName('System.Web.Extensions')
   return ParseItem -jsonItem ((New-Object -TypeName System.Web.Script.Serialization.JavaScriptSerializer -Property @{
         MaxJsonLength = 67108864
   }).DeserializeObject($response.Content))
 }
-function ParseItem($jsonItem) 
-{
-  if($jsonItem.PSObject.TypeNames -match 'Array') 
-  {
+
+
+function ParseItem($jsonItem) {
+<#
+.SYNOPSIS
+Main function that determines the type of object and calls 
+#>
+  if($jsonItem.PSObject.TypeNames -match 'Array') {
     return ParseJsonArray -jsonArray ($jsonItem)
-  }
-  elseif($jsonItem.PSObject.TypeNames -match 'Dictionary') 
-  {
+  } elseif($jsonItem.PSObject.TypeNames -match 'Dictionary') {
     return ParseJsonObject -jsonObj ([HashTable]$jsonItem)
-  }
-  else 
-  {
+  } else  {
     return $jsonItem
   }
 }
 
-function ParseJsonObject($jsonObj) 
-{
+function ParseJsonObject($jsonObj) {
+<#
+.SYNOPSIS
+
+#>
   $result = New-Object -TypeName PSCustomObject
   foreach ($key in $jsonObj.Keys) 
   {
     $item = $jsonObj[$key]
-    if ($null -ne $item) 
-    {
+
+    if ($null -ne $item) {
       $parsedItem = ParseItem -jsonItem $item
-    }
-    else 
-    {
+    } else {
       $parsedItem = $null
     }
+
     $result | Add-Member -MemberType NoteProperty -Name $key -Value $parsedItem
   }
   return $result
 }
 
-function ParseJsonArray($jsonArray) 
-{
+function ParseJsonArray($jsonArray) {
+<#
+.SYNOPSIS
+Expands the array and feeds this back into ParseItem
+#>
   $result = @()
   $jsonArray | ForEach-Object -Process {
     $result += , (ParseItem -jsonItem $_)
   }
   return $result
-}
-
-function ParseJsonString($json) 
-{
-  $config = $javaScriptSerializer.DeserializeObject($json)
-  return ParseJsonObject -jsonObj ($config)
 }
