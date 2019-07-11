3548a2b65361999be89d0d39dc1c333b7952c5ae
Mike Preston <mwpreston@gmail.com>
2019-07-10 22:35:00 -0400

    more fixes to Get-RubrikManagedVolume Unit Test

--- Tests/Get-RubrikManagedVolume.Tests.ps1
+++ Tests/Get-RubrikManagedVolume.Tests.ps1
@@ -21,61 +21,54 @@ Describe -Name 'Public/Get-RubrikManagedVolume' -Tag 'Public', 'Get-RubrikManage
 
     Context -Name 'Results Filtering' {
         Mock -CommandName Test-RubrikConnection -Verifiable -ModuleName 'Rubrik' -MockWith {}
+        Mock -CommandName Test-RubrikSLA -Verifiable -ModuleName 'Rubrik' -MockWith {
+            @{ 'slaid' = '12345678-1234-abcd-8910-1234567890ab' }
+        }
         Mock -CommandName Submit-Request -Verifiable -ModuleName 'Rubrik' -MockWith {
-            @{ 
+            @{
                 'id'                        = 'ManagedVolume:::11111'
-                'name'                      = 'OracleMV1'
-                'state'                     = 'Exported'
-                'primaryClusterId'          = 'cluster01'
-                'configuredSlaDomainName'   = 'Gold'
+                'isDeleted'                 = 'False'
+                'primaryClusterId'          = 'local'
+                'name'                      = 'OracleMV'
                 'isRelic'                   = 'False'
-                'effectiveSlaDomainId'      = 'test-sla_id'
+                'effectiveSlaDomainId'      = '12345678-1234-abcd-8910-1234567890ab'
+                'configuredSlaDomainId'     = '12345678-1234-abcd-8910-1234567890ab'
+                'effectiveSlaDomainName'    = 'Gold'
+                'configuredSlaDomainName'   = 'Gold'
             },
-            @{ 
+            @{
                 'id'                        = 'ManagedVolume:::22222'
+                'isDeleted'                 = 'False'
+                'primaryClusterId'          = 'local'
                 'name'                      = 'SQLMV'
-                'state'                     = 'Exported'
-                'primaryClusterId'          = 'cluster01'
-                'configuredSlaDomainName'   = 'Gold'
                 'isRelic'                   = 'False'
-                'effectiveSlaDomainId'      = 'test-sla_id'
-            },
-            @{ 
-                'id'                        = 'ManagedVolume:::33333'
-                'name'                      = 'Postgres'
-                'state'                     = 'Exported'
-                'primaryClusterId'          = 'cluster02'
-                'configuredSlaDomainName'   = 'Silver'
-                'isRelic'                   = 'True'
-                'effectiveSlaDomainId'      = 'test-sla_id2'
-            },
-            @{ 
-                'id'                        = 'ManagedVolume:::44444'
-                'name'                      = 'OracleMV2'
-                'state'                     = 'Exported'
-                'primaryClusterId'          = 'cluster01'
-                'configuredSlaDomainName'   = 'Silver'
-                'isRelic'                   = 'False'
-                'effectiveSlaDomainId'      = 'test-sla_id2'
-            },
-            @{ 
-                'id'                        = 'ManagedVolume:::55555'
-                'name'                      = 'OracleMV3'
-                'state'                     = 'Exported'
-                'primaryClusterId'          = 'cluster01'
-                'configuredSlaDomainName'   = 'Gold'
-                'isRelic'                   = 'True'
-                'effectiveSlaDomainId'      = 'test-sla_id'
+                'effectiveSlaDomainId'      = '12345678-1234-abcd-8910-1234567890abc'
+                'configuredSlaDomainId'     = '12345678-1234-abcd-8910-1234567890abc'
+                'effectiveSlaDomainName'    = 'Bronze'
+                'configuredSlaDomainName'   = 'Bronze'
             }
         }
-        It -Name 'Should Return count of 5' -Test {
+        
+        It -Name 'Should Return count of 2' -Test {
             (Get-RubrikManagedVolume).Count |
-                Should -BeExactly 5
+                Should -BeExactly 2
         }
         It -Name 'Name filter non existant should return count of 0' -Test {
             (Get-RubrikManagedVolume -Name 'nonexistant').Count |
                 Should -BeExactly 0
-        }  
+        } 
+        It -Name 'Name Filter - ID should be ManagedVolume:::22222' -Test {
+            (Get-RubrikManagedVolume -name 'SQLMV').id |
+                Should -BeExactly 'ManagedVolume:::22222'
+        } 
+        It -Name 'SLA Filter should be count of 1' -Test {
+            (Get-RubrikManagedVolume -SLA "Bronze" | Measure-Object).Count |
+                Should -BeExactly 1
+        } 
+        It -Name 'Missing ID Exception' -Test {
+            { Get-RubrikManagedVolume -id  } |
+                Should -Throw "Missing an argument for parameter 'id'. Specify a parameter of type 'System.String' and try again."
+        } 
         Assert-VerifiableMock
         Assert-MockCalled -CommandName Test-RubrikConnection -ModuleName 'Rubrik' -Times 1
         Assert-MockCalled -CommandName Submit-Request -ModuleName 'Rubrik' -Times 1
