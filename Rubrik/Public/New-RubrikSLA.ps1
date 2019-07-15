a7fa2b84a8373b513c0bd46d61fb6923ec347dd2
Jaap Brasser <noreply@jaapbrasser.com>
2019-07-16 00:36:48 +0200

    Fixed Begin block Spacing

--- Rubrik/Public/New-RubrikSLA.ps1
+++ Rubrik/Public/New-RubrikSLA.ps1
@@ -95,7 +95,7 @@ function New-RubrikSLA
     [String]$api = $global:RubrikConnection.api
   )
 
-    Begin {
+  Begin {
 
     # The Begin section is used to perform one-time loads of data necessary to carry out the function's purpose
     # If a command needs to be run with each iteration or pipeline input, place it in the Process section
