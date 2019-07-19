4ff2be4250745d25dde6ed94e127081353ed2e93
Mike Preston <mwpreston@gmail.com>
2019-07-19 09:41:52 -0400

    tests for Get-RubrikVersion and update changelog

---
+++ Tests/Get-RubrikVersion.Tests.ps1
@@ -0,0 +1,44 @@
+Remove-Module -Name 'Rubrik' -ErrorAction 'SilentlyContinue'
+Import-Module -Name './Rubrik/Rubrik.psd1' -Force
+
+foreach ( $privateFunctionFilePath in ( Get-ChildItem -Path './Rubrik/Private' | Where-Object extension -eq '.ps1').FullName  ) {
+    . $privateFunctionFilePath
+}
+
+Describe -Name 'Public/Get-RubrikVersion' -Tag 'Public', 'Get-RubrikVersion' -Fixture {
+    #region init
+    $global:rubrikConnection = @{
+        id      = 'test-id'
+        userId  = 'test-userId'
+        token   = 'test-token'
+        server  = 'test-server'
+        header  = @{ 'Authorization' = 'Bearer test-authorization' }
+        time    = (Get-Date)
+        api     = 'v1'
+        version = '4.0.5'
+    }
+    #endregion
+
+    Context -Name 'Results Filtering' {
+        Mock -CommandName Test-RubrikConnection -Verifiable -ModuleName 'Rubrik' -MockWith {}
+        Mock -CommandName Submit-Request -Verifiable -ModuleName 'Rubrik' -MockWith {
+            @{ 
+                'name'                   = 'Cluster_B'
+                'id'                     = '111111'
+                'version'                = '5.0.1-p2-1592'
+                'apiVersion'             = '1'
+            }
+        }
+        It -Name 'Should Return count of 4' -Test {
+            ( Get-RubrikVersion).Count |
+                Should -BeExactly 4
+        }
+        It -Name 'Should Return Cluster_B' -Test {
+            (Get-RubrikVersion).Name |
+                Should -BeExactly 'Cluster_B'
+        }
+        Assert-VerifiableMock
+        Assert-MockCalled -CommandName Test-RubrikConnection -ModuleName 'Rubrik' -Times 1
+        Assert-MockCalled -CommandName Submit-Request -ModuleName 'Rubrik' -Times 1
+    }
+}
