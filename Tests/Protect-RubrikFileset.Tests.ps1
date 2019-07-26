c7f4a20b4261dde966d5fa29bd50a0499a52333c
Mike Preston <mwpreston@gmail.com>
2019-07-26 10:45:55 -0400

    Tests for Protect-RubrikFileset

---
+++ Tests/Protect-RubrikFileset.Tests.ps1
@@ -0,0 +1,56 @@
+Remove-Module -Name 'Rubrik' -ErrorAction 'SilentlyContinue'
+Import-Module -Name './Rubrik/Rubrik.psd1' -Force
+
+foreach ( $privateFunctionFilePath in ( Get-ChildItem -Path './Rubrik/Private' | Where-Object extension -eq '.ps1').FullName  ) {
+    . $privateFunctionFilePath
+}
+
+Describe -Name 'Public/Protect-RubrikFileset' -Tag 'Public', 'Protect-RubrikFileset' -Fixture {
+    #region init
+    $global:rubrikConnection = @{
+        id      = 'test-id'
+        userId  = 'test-userId'
+        token   = 'test-token'
+        server  = 'test-server'
+        header  = @{ 'Authorization' = 'Bearer test-authorization' }
+        time    = (Get-Date)
+        api     = 'v1'
+        version = '4.0.5'
+    }
+    #endregion
+
+    Context -Name 'Returned Results' {
+        Mock -CommandName Test-RubrikConnection -Verifiable -ModuleName 'Rubrik' -MockWith {}
+        Mock -CommandName Test-RubrikSLA -Verifiable -ModuleName 'Rubrik' -MockWith {
+            @{ 'id' = 'sla_domain_id' }
+        }
+        Mock -CommandName Submit-Request -Verifiable -ModuleName 'Rubrik' -MockWith {
+            @{ 
+                'name'                   = 'Foo'
+                'id'                     = 'Fileset::111111-2222-3333-4444-555555555555'
+                'effectiveSlaDomainId'   = 'sla_domain_id'
+                'useWindowsVss'          = 'False'
+                'shareCount'             = '0'
+                'primaryClusterId'       = 'cluster_id1' 
+            }
+        }
+        It -Name 'Fileset assigned to SLA Domain' -Test {
+            ( Protect-RubrikFileset -id 'filesetid' -SLA 'Gold').effectiveSlaDomainId |
+                Should -BeExactly 'sla_domain_id'
+        }  
+        Assert-VerifiableMock
+        Assert-MockCalled -CommandName Test-RubrikConnection -ModuleName 'Rubrik' -Times 1
+        Assert-MockCalled -CommandName Test-RubrikSLA -ModuleName 'Rubrik' -Times 1
+        Assert-MockCalled -CommandName Submit-Request -ModuleName 'Rubrik' -Times 1
+    }
+    Context -Name 'Parameter Validation' {   
+        It -Name 'ID cannot be null' -Test {
+            { Protect-RubrikFileset -Id $null -SLA 'Gold' } |
+                Should -Throw "Cannot bind argument to parameter 'id' because it is an empty string."
+        }
+        It -Name 'Missing SLA' -Test {
+            { Protect-RubrikFileset -Id 'filesetID' -SLA  } |
+                Should -Throw "Missing an argument for parameter 'SLA'."
+        }
+    }
+}
