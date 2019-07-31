32ad7d6fb8320c5205e6baee72a802d633bb1f84
Jaap Brasser <noreply@jaapbrasser.com>
2019-07-31 19:10:32 +0200

    Consolidated parameter sets names

--- Rubrik/Public/Get-RubrikFileset.ps1
+++ Rubrik/Public/Get-RubrikFileset.ps1
@@ -53,18 +53,18 @@ function Get-RubrikFileset
     [ValidateNotNullOrEmpty()]
     [Alias('Fileset')]
     [String]$Name,
-    [Parameter(ParameterSetName='NameFilter')]
+    [Parameter(ParameterSetName='Filter')]
     [ValidateNotNullOrEmpty()]
-    [String]$NameFilter,
+    [String]$Filter,
     # Exact name of the host using a fileset
     [Parameter(ParameterSetName='Query')]
     [Alias('host_name')]
     [ValidateNotNullOrEmpty()]
     # Partial match of hostname, using an 'in fix' search.
     [String]$HostName,
-    [Parameter(ParameterSetName='HostFilter')]
+    [Parameter(ParameterSetName='Filter')]
     [ValidateNotNullOrEmpty()]
-    [String]$HostNameFilter,
+    [String]$HostFilter,
     # Rubrik's fileset id
     [Parameter(ParameterSetName='ID')]
     [Parameter(ValueFromPipelineByPropertyName = $true)]    
@@ -73,58 +73,49 @@ function Get-RubrikFileset
     # Filter results to include only relic (removed) filesets
     [Alias('is_relic')]
     [Parameter(ParameterSetName='Query')]
-    [Parameter(ParameterSetName='NameFilter')]
-    [Parameter(ParameterSetName='HostFilter')]
+    [Parameter(ParameterSetName='Filter')]
     [Switch]$Relic,
     # DetailedObject will retrieved the detailed VM object, the default behavior of the API is to only retrieve a subset of the full Fileset object unless we query directly by ID. Using this parameter does affect performance as more data will be retrieved and more API-queries will be performed.
     [Parameter(ParameterSetName='Query')]
-    [Parameter(ParameterSetName='NameFilter')]
-    [Parameter(ParameterSetName='HostFilter')]
+    [Parameter(ParameterSetName='Filter')]
     [switch]$DetailedObject,
     # SLA Domain policy assigned to the database
     [Parameter(ParameterSetName='Query')]
-    [Parameter(ParameterSetName='NameFilter')]
-    [Parameter(ParameterSetName='HostFilter')]
+    [Parameter(ParameterSetName='Filter')]
     [ValidateNotNullOrEmpty()]
     [String]$SLA,
     # Filter the summary information based on the ID of a fileset template.
     [Parameter(ParameterSetName='Query')]
-    [Parameter(ParameterSetName='NameFilter')]
-    [Parameter(ParameterSetName='HostFilter')]
+    [Parameter(ParameterSetName='Filter')]
     [Alias('template_id')]
     [ValidateNotNullOrEmpty()]
     [String]$TemplateID,
     # Filter the summary information based on the primarycluster_id of the primary Rubrik cluster. Use **_local** as the primary_cluster_id of the Rubrik cluster that is hosting the current REST API session.
     [Parameter(ParameterSetName='Query')]
-    [Parameter(ParameterSetName='NameFilter')]
-    [Parameter(ParameterSetName='HostFilter')]
+    [Parameter(ParameterSetName='Filter')]
     [Alias('primary_cluster_id')]
     [ValidateNotNullOrEmpty()]
     [String]$PrimaryClusterID,
     # Rubrik's Share id
     [Parameter(ParameterSetName='Query')]
-    [Parameter(ParameterSetName='NameFilter')]
-    [Parameter(ParameterSetName='HostFilter')]
+    [Parameter(ParameterSetName='Filter')]
     [Alias('share_id')]
     [ValidateNotNullOrEmpty()]
     [String]$ShareID,
     # SLA id value
     [Parameter(ParameterSetName='Query')]
-    [Parameter(ParameterSetName='NameFilter')]
-    [Parameter(ParameterSetName='HostFilter')]
+    [Parameter(ParameterSetName='Filter')]
     [Alias('effective_sla_domain_id')]
     [ValidateNotNullOrEmpty()]
     [String]$SLAID,
     # Rubrik server IP or FQDN
     [Parameter(ParameterSetName='Query')]
-    [Parameter(ParameterSetName='NameFilter')]
-    [Parameter(ParameterSetName='HostFilter')]
+    [Parameter(ParameterSetName='Filter')]
     [Parameter(ParameterSetName='ID')]
     [String]$Server = $global:RubrikConnection.server,
     # API version
     [Parameter(ParameterSetName='Query')]
-    [Parameter(ParameterSetName='NameFilter')]
-    [Parameter(ParameterSetName='HostFilter')]
+    [Parameter(ParameterSetName='Filter')]
     [Parameter(ParameterSetName='ID')]
     [String]$api = $global:RubrikConnection.api
   )
