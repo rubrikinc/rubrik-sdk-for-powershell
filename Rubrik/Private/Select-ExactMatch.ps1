c4ba58f664f0a91f4318580d647388d581aabbf4
Jaap Brasser <noreply@jaapbrasser.com>
2019-08-12 00:30:52 +0200

    Moved verbose message

--- Rubrik/Private/Select-ExactMatch.ps1
+++ Rubrik/Private/Select-ExactMatch.ps1
@@ -1,4 +1,5 @@
 function Select-ExactMatch {
+    [CmdletBinding()]
     param (
         [parameter(Mandatory)]
         [string] $Parameter,
@@ -11,8 +12,8 @@ function Select-ExactMatch {
     process {
         $OldCount = @($Result).count
 
-        return $result | Where-Object {$Value -eq $_.$Parameter}
-
         Write-Verbose ('Excluded results not matching -{0} ''{1}'' {2} object(s) filtered, {3} object(s) remaining' -f $Parameter,$Value,($OldCount-@($Result).count),@($Result).count)
+
+        return $result | Where-Object {$Value -eq $_.$Parameter}
     }
 }
