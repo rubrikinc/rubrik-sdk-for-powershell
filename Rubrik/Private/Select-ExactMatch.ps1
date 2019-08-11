ec687e3792f3366248db2aefcdbf86157b9890c9
Jaap Brasser <noreply@jaapbrasser.com>
2019-08-12 00:16:01 +0200

    Created param block

--- Rubrik/Private/Select-ExactMatch.ps1
+++ Rubrik/Private/Select-ExactMatch.ps1
@@ -1,7 +1,16 @@
-function Select-ExactMatch.ps1 {
+function Select-ExactMatch {
     param (
-
+        [parameter(Mandatory)]
+        [string] $Parameter,
+        [parameter(Mandatory)]
+        [string] $Value
     )
 
-    
+    if ($null -ne $PSBoundParameters.Name) {
+        $OldCount = @($Result).count
+
+        $result = $result | Where-Object {$Name -eq $_.name}
+
+        Write-Verbose ('Excluded results not matching -Name ''{0}'' {1} object(s) filtered, {2} object(s) remaining' -f $Name,($OldCount-@($Result).count),@($Result).count)
+      }
 }
