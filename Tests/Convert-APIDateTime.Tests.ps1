435b4c5455f07b9fab43646181e8cdb581802b53
Jaap Brasser <noreply@jaapbrasser.com>
2019-08-13 11:33:01 +0200

    Added test cases

--- Tests/Convert-APIDateTime.Tests.ps1
+++ Tests/Convert-APIDateTime.Tests.ps1
@@ -7,11 +7,7 @@ foreach ( $privateFunctionFilePath in ( Get-ChildItem -Path './Rubrik/Private' |
 
 Describe -Name 'Private/Convert-APIDateTime' -Tag 'Private', 'Convert-APIDateTime' -Fixture {
     Context -Name 'Results Filtering' {
-        It -Name 'Should convert date correctly' -Test {
-            (Convert-APIDateTime).Count |
-                Should -BeExactly 5
-        }    
-        
+      
         $cases = 'Mon Jan 10 17:12:14 UTC 2019',
         'Mon Mar 11 09:12:14 UTC 2017',
         'Mon Sep 12 23:12:14 UTC 2018',
@@ -20,8 +16,10 @@ Describe -Name 'Private/Convert-APIDateTime' -Tag 'Private', 'Convert-APIDateTim
             
         }
 
-        It -Name "Get-RubrikAPIData - <f> test" -TestCases $cases {
-            
+        It -Name "Get-RubrikAPIData - <DateTimeString> test" -TestCases $cases {
+            param($DateTimeString)
+            Convert-APIDateTime -DateTimeString $DateTimeString.ToString() |
+                Should -BeOfType DateTime
         }
     }
 }
