932a05f2c30a70ac3afcec7cf2539d1455184f59
Jaap Brasser <noreply@jaapbrasser.com>
2019-08-13 11:22:00 +0200

    Created test cases

--- Tests/Convert-APIDateTime.Tests.ps1
+++ Tests/Convert-APIDateTime.Tests.ps1
@@ -5,73 +5,23 @@ foreach ( $privateFunctionFilePath in ( Get-ChildItem -Path './Rubrik/Private' |
     . $privateFunctionFilePath
 }
 
-Describe -Name 'Public/Get-RubrikEvent' -Tag 'Public', 'Get-RubrikEvent' -Fixture {
-    #region init
-    $global:rubrikConnection = @{
-        id      = 'test-id'
-        userId  = 'test-userId'
-        token   = 'test-token'
-        server  = 'test-server'
-        header  = @{ 'Authorization' = 'Bearer test-authorization' }
-        time    = (Get-Date)
-        api     = 'v1'
-        version = '4.0.5'
-    }
-    #endregion
-
+Describe -Name 'Private/Convert-APIDateTime' -Tag 'Private', 'Convert-APIDateTime' -Fixture {
     Context -Name 'Results Filtering' {
-        Mock -CommandName Test-RubrikConnection -Verifiable -ModuleName 'Rubrik' -MockWith {}
-        Mock -CommandName Submit-Request -Verifiable -ModuleName 'Rubrik' -MockWith {
-            @{ 
-                'name'                   = 'VirtualMachine01'
-                'id'                     = 'VirtualMachine:::11111'
-                'eventType'              = 'Replication'
-                'time'                   = 'Mon Aug 10 07:12:14 UTC 2019'
-            },
-            @{ 
-                'name'                   = 'VirtualMachine02'
-                'id'                     = 'VirtualMachine:::22222'
-                'eventType'              = 'Backup'
-                'time'                   = 'Mon Aug 11 07:12:14 UTC 2019'
-            },
-            @{ 
-                'name'                   = 'VirtualMachine03'
-                'id'                     = 'VirtualMachine:::33333'
-                'eventType'              = 'CloudNativeSource'
-                'time'                   = 'Mon Aug 12 07:12:14 UTC 2019'
-            },
-            @{ 
-                'name'                   = 'VirtualMachine04'
-                'id'                     = 'VirtualMachine:::44444'
-                'eventType'              = 'Replication'
-                'time'                   = 'Mon Aug 13 07:12:14 UTC 2019'
-            },
-            @{ 
-                'name'                   = 'VirtualMachine05'
-                'id'                     = 'VirtualMachine:::55555'
-                'eventType'              = 'Replication'
-                'time'                   = 'Mon Aug 14 07:12:14 UTC 2019'
-            }
-        }
-        It -Name 'Should Return count of 5' -Test {
-            (Get-RubrikEvent).Count |
+        It -Name 'Should convert date correctly' -Test {
+            (Convert-APIDateTime).Count |
                 Should -BeExactly 5
-        }
-        It -Name 'Should not run with Name parameter' -Test {
-            {Get-RubrikEvent -Name doesnotexist -ErrorAction Stop} |
-                Should -Throw
-        }
-        It -Name 'Time property should be a string' -Test {
-            (Get-RubrikEvent)[0].Time |
-                Should -BeOfType String
-        }
-        It -Name 'Date property should be a datetime object' -Test {
-            (Get-RubrikEvent)[0].Date |
-                Should -BeOfType DateTime
-        }
+        }    
         
-        Assert-VerifiableMock
-        Assert-MockCalled -CommandName Test-RubrikConnection -ModuleName 'Rubrik' -Exactly 3
-        Assert-MockCalled -CommandName Submit-Request -ModuleName 'Rubrik' -Exactly 3
+        $cases = 'Mon Jan 10 17:12:14 UTC 2019',
+        'Mon Mar 11 09:12:14 UTC 2017',
+        'Mon Sep 12 23:12:14 UTC 2018',
+        'Mon Dec 13 14:12:14 UTC 2006' | ForEach-Object {
+            @{'DateTimeString' = $_}
+            
+        }
+
+        It -Name "Get-RubrikAPIData - <f> test" -TestCases $cases {
+            
+        }
     }
 }
