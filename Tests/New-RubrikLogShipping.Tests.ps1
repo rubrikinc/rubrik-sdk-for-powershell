dba2ec16cf77b40ad65e26774e7eb3d2655c12d2
Jaap Brasser <noreply@jaapbrasser.com>
2019-08-30 17:00:46 -0700

    Removed manual loading of private functions

--- Tests/New-RubrikLogShipping.Tests.ps1
+++ Tests/New-RubrikLogShipping.Tests.ps1
@@ -1,10 +1,6 @@
 Remove-Module -Name 'Rubrik' -ErrorAction 'SilentlyContinue'
 Import-Module -Name './Rubrik/Rubrik.psd1' -Force
 
-#foreach ( $privateFunctionFilePath in ( Get-ChildItem -Path './Rubrik/Private' | Where-Object extension -eq '.ps1').FullName  ) {
-#    . $privateFunctionFilePath
-#}
-
 Describe -Name 'Public/New-RubrikLogShipping' -Tag 'Public', 'New-RubrikLogShipping' -Fixture {
     InModuleScope -ModuleName Rubrik -ScriptBlock {
         #region init
