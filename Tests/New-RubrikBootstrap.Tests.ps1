e90e2aaeada8aa1a5aba30a8be9998779883e1f7
Jaap Brasser <noreply@jaapbrasser.com>
2019-09-01 23:03:51 -0700

    Created first test

--- Tests/New-RubrikBootstrap.Tests.ps1
+++ Tests/New-RubrikBootstrap.Tests.ps1
@@ -14,31 +14,28 @@ Describe -Name 'Public/New-RubrikBootstrap' -Tag 'Public', 'New-RubrikBootstrap'
             api     = 'v1'
             version = '4.0.5'
         }
+        
+        $BootStrapHash = @{
+            name = 'rubrik-edge' 
+            dnsNameservers = @('192.168.11.1')
+            dnsSearchDomains = @('corp.us','branch.corp.us')
+            ntpserverconfigs = @(@{server = 'pool.ntp.org'})
+            adminUserInfo = @{emailAddress = 'nick@shoresmedia.com'; id ='admin'; password = 'P@SSw0rd!'}
+            nodeconfigs = @{node1 = @{managementIpConfig = @{address = '192.168.11.1'; gateway = '192.168.11.100'; netmask = '255.255.255.0'}}}
+        }
         #endregion
 
         Context -Name 'New Log Shipping Results' {
-            Mock -CommandName Test-RubrikConnection -Verifiable -ModuleName 'Rubrik' -MockWith {}
             Mock -CommandName Submit-Request -Verifiable -ModuleName 'Rubrik' -MockWith {
-                @{
-                    'id'       = 'RESTORE_MSSQL_DB_11111'
-                    'status'   = 'QUEUED'
-                    'progress' = '0'
-                }
+                $true
             }
 
-            It -Name 'Should invoke new log shipping restore with QUEUED status' -Test {
-                ( New-RubrikBootstrap -Id 'databaseid' -targetInstanceId 'instance' -targetDatabaseName 'db').status |
-                    Should -BeExactly 'QUEUED'
-            }
-            
-            It -Name 'Should invoke new log shipping restore correct ID' -Test {
-                ( New-RubrikBootstrap -Id 'databaseid' -targetInstanceId 'instance' -targetDatabaseName 'db').id |
-                    Should -BeExactly 'RESTORE_MSSQL_DB_11111'
+            It -Name 'Should run without error' -Test {
+                ( New-RubrikBootstrap @BootStrapHash ) | Should -BeExactly $true
             }
             
             Assert-VerifiableMock
-            Assert-MockCalled -CommandName Test-RubrikConnection -ModuleName 'Rubrik' -Exactly 2
-            Assert-MockCalled -CommandName Submit-Request -ModuleName 'Rubrik'  -Exactly 2
+            Assert-MockCalled -CommandName Submit-Request -ModuleName 'Rubrik'  -Exactly 1
         }
         Context -Name 'Parameter Validation' {
             # TODO
