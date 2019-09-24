7dff3cdbee96f5530d9142dba7a179d85335c071
Arnaud Alcabas <aalcabas@fr.scc.com>
2019-09-24 09:20:42 +0200

    Fix TLS version not working using powershell 5.1

--- Rubrik/Private/Unblock-SelfSignedCert.ps1
+++ Rubrik/Private/Unblock-SelfSignedCert.ps1
@@ -5,20 +5,23 @@
 function Unblock-SelfSignedCert() 
 {
   Write-Verbose -Message 'Allowing self-signed certificates'
-    
+  
   if ([System.Net.ServicePointManager]::CertificatePolicy -notlike 'TrustAllCertsPolicy') 
   {
     Add-Type -TypeDefinition @"
     using System.Net;
     using System.Security.Cryptography.X509Certificates;
     public class TrustAllCertsPolicy : ICertificatePolicy {
-	    public bool CheckValidationResult(
-	        ServicePoint srvPoint, X509Certificate certificate,
-	        WebRequest request, int certificateProblem) {
-	        return true;
-	    }
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
     }
 "@
     [System.Net.ServicePointManager]::CertificatePolicy = New-Object -TypeName TrustAllCertsPolicy
   }
+  
+  [System.Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+  
 }
