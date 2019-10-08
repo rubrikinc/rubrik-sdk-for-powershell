a367e88a6e0ef0424de383a4b6272c7b26d61ffd
Jaap Brasser <noreply@jaapbrasser.com>
2019-10-08 09:11:50 +0200

    Updated docs.ps1 to manually generate summary.md

--- azure-pipelines/scripts/docs.ps1
+++ azure-pipelines/scripts/docs.ps1
@@ -1,24 +1,67 @@
-    # Create new markdown and XML help files
+# Create new markdown and XML help files
 Write-Host "Building new function documentation" -ForegroundColor Yellow
 New-MarkdownHelp -Module Rubrik -OutputFolder "$env:LocalPath\docs\reference\" -Force
 New-ExternalHelp -Path "$env:LocalPath\docs\reference\" -OutputPath "$env:LocalPath\Rubrik\en-US\" -Force
 
+# Custom Generate Summary.md
+$MarkDown = "# Rubrik SDK for PowerShell`n`n"
+$MarkDown += "## User Documentation`n`n"
 
+# Documentation folder
+Get-ChildItem -LiteralPath "$env:LocalPath\docs\documentation" | ForEach-Object -Process {
+    $Reference = switch ($_.BaseName) {
+        'Requirements' {'Requirements'}
+        'Installation' {'Installation'}
+        'Getting_Started' {'Getting Started'}
+        'Project_Architecture' {'Project Architecture'}
+        'Support' {'Support'}
+        'Contribution' {'Contribution'}
+        'Licensing' {'Licensing'}
+        'FAQ' {'FAQ'}
+        default {$_}
+    }
+    $uri = "$($_.Directory.BaseName)/$($_.Name)"
+    $MarkDown += "* [$Reference]($uri)`n"
+} -End {
+    $MarkDown += "`n----`n`n"
+}
 
-$StageFolder = '.\compiled_docs'
+$MarkDown += "## User Documentation`n`n"
 
-# Create a Staging folder and copy items in for compiling
-$null = New-Item -Path $StageFolder -ItemType:Directory
-Copy-Item -Path '.\*.md' -Destination $StageFolder -Force
-Copy-Item -Path '.\docs\*' -Destination $StageFolder -Recurse -Force
+# Workflow folder
+Get-ChildItem -LiteralPath "$env:LocalPath\docs\workflow" -Exclude 'readme.md' | ForEach-Object -Begin {
+    $MarkDown += "* [Workflow](workflow/readme.md)`n"
+} -Process {
+    $Reference = switch ($_.BaseName) {
+        'flow_audit' {'Flow Audit'}
+        default {$_}
+    }
+    $uri = "$($_.Directory.BaseName)/$($_.Name)"
+    
+    if ($_.basename -eq 'readme') {
+        $MarkDown += "* [$Reference]($uri)`n"
+    } else {
+        $MarkDown += "    * [$Reference]($uri)`n"
+    }
+} -End {
+    $MarkDown += "`n"
+}
 
-# Build the book
-Push-Location -Path $StageFolder
-gitbook install
-gitbook build
-Pop-Location
+# Reference folder
+Get-ChildItem -LiteralPath "$env:LocalPath\docs\reference" -Exclude 'readme.md' | ForEach-Object -Begin {
+    $MarkDown += "* [Reference](workflow/readme.md)`n"
+} -Process {
+    $Reference = switch ($_.BaseName) {
+        default {$_}
+    }
+    $uri = "$($_.Directory.BaseName)/$($_.Name)"
+    
+    if ($_.BaseName -eq 'readme') {
+        $MarkDown += "* [$Reference]($uri)`n"
+    } else {
+        $MarkDown += "    * [$Reference]($uri)`n"
+    }
+}
 
-# Copy the new book items into the docs folder
-Remove-Item -Path '.\docs\' -Include *.html -Recurse -Force
-Copy-Item -Path "$StageFolder\_book\*" -Destination '.\docs\' -Recurse -Force
-Remove-Item -Path $StageFolder -Recurse -Force
+# Write Markdown to file
+Set-Content -Value $MarkDown -Path "$env:LocalPath\docs\SUMMARY.md"
