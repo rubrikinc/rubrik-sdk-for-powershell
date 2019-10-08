b3af2d8070c2a546391ce1d22132d636bc9afa0d
Jaap Brasser <noreply@jaapbrasser.com>
2019-10-08 05:41:24 +0200

    Updated versioning

--- azure-pipelines/scripts/build.ps1
+++ azure-pipelines/scripts/build.ps1
@@ -17,7 +17,7 @@ if ($env:targetBranch -ne 'master') {
         $manifest = Test-ModuleManifest -Path $manifestPath
         [System.Version]$version = $manifest.Version
         Write-Output "Old Version: $version"
-        [String]$newVersion = New-Object -TypeName System.Version -ArgumentList ($version.Major, $version.Minor, $version.Build, $env:PRNumber)
+        [String]$newVersion = New-Object -TypeName System.Version -ArgumentList ($version.Major, $version.Minor, $manifest.version.revision+1)
         Write-Output "New Version: $newVersion"
 
         # Update the manifest with the new version value and fix the weird string replace bug
