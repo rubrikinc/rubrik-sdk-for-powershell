4877c013be9e9918fa63bed080580a08f0bf6b6a
Mike Preston <mwpreston@gmail.com>
2019-10-22 11:59:05 -0400

    scoped $id variable to local

--- Rubrik/Public/New-RubrikHost.ps1
+++ Rubrik/Public/New-RubrikHost.ps1
@@ -61,8 +61,8 @@ function New-RubrikHost
   }
 
   Process {
-
-    $uri = New-URIString -server $Server -endpoint ($resources.URI) -id $id
+    
+    $uri = New-URIString -server $Server -endpoint ($resources.URI) -id $local:id
     $uri = Test-QueryParam -querykeys ($resources.Query.Keys) -parameters ((Get-Command $function).Parameters.Values) -uri $uri
     $body = New-BodyString -bodykeys ($resources.Body.Keys) -parameters ((Get-Command $function).Parameters.Values)
     $result = Submit-Request -uri $uri -header $Header -method $($resources.Method) -body $body
