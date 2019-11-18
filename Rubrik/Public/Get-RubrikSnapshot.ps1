d936324f6a243d8866154f1c2c5e6eefa52c1958
Jaap Brasser <noreply@jaapbrasser.com>
2019-11-18 14:21:17 +0100

    Removed line break

--- Rubrik/Public/Get-RubrikSnapshot.ps1
+++ Rubrik/Public/Get-RubrikSnapshot.ps1
@@ -115,7 +115,6 @@ function Get-RubrikSnapshot
       $result = Submit-Request -uri $uri -header $Header -method $($resources.Method) -body $body
       $result = Test-ReturnFormat -api $api -result $result -location $resources.Result
     }    
-
     
     $result = Test-FilterObject -filter ($resources.Filter) -result $result
     
