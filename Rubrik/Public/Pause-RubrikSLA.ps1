70796e7e821f7bf22ce957884ccdc9c43e79e30f
Jaap Brasser <noreply@jaapbrasser.com>
2020-03-07 19:19:26 +0100

    Added initial Pause-RubrikSLA function

---
+++ Rubrik/Public/Pause-RubrikSLA.ps1
@@ -0,0 +1,75 @@
+#Requires -Version 3
+function Pause-RubrikSLA
+{
+  <#  
+      .SYNOPSIS
+      Updates an existing Rubrik SLA Domain
+
+      .DESCRIPTION
+      The Set-RubrikSLA cmdlet will update an existing SLA Domain with specified parameters.
+
+      .NOTES
+      Written by Pierre-François Guglielmi for community usage
+      Twitter: @pfguglielmi
+      GitHub: pfguglielmi
+
+      .LINK
+      https://rubrik.gitbook.io/rubrik-sdk-for-powershell/command-documentation/reference/pause-rubriksla
+
+      .EXAMPLE
+      Get-RubrikSLA -Name Gold | Set-RubrikSLA -Replication:$false
+      This will update the SLA Domain named "Gold" to only disable replication
+  #>
+
+  [CmdletBinding(SupportsShouldProcess = $true,ConfirmImpact = 'High')]
+  Param(
+    # SLA id value from the Rubrik Cluster
+    [Parameter(
+      ValueFromPipelineByPropertyName = $true,
+      Mandatory = $true )]
+    [ValidateNotNullOrEmpty()]
+    [String]$id,
+    # SLA Domain Name
+    [Parameter(
+      ValueFromPipelineByPropertyName = $true)]
+    [Alias('SLA')]
+    [ValidateNotNullOrEmpty()]
+    [String]$Name,
+    # Rubrik server IP or FQDN
+    [String]$Server = $global:RubrikConnection.server,
+    # API version
+    [String]$api = $global:RubrikConnection.api
+  )
+
+  Begin {
+
+    # The Begin section is used to perform one-time loads of data necessary to carry out the function's purpose
+    # If a command needs to be run with each iteration or pipeline input, place it in the Process section
+    
+    # Check to ensure that a session to the Rubrik cluster exists and load the needed header data for authentication
+    Test-RubrikConnection
+    
+    # API data references the name of the function
+    # For convenience, that name is saved here to $function
+    $function = $MyInvocation.MyCommand.Name
+        
+    # Retrieve all of the URI, method, body, query, result, filter, and success details for the API endpoint
+    Write-Verbose -Message "Gather API Data for $function"
+    $resources = Get-RubrikAPIData -endpoint $function
+    Write-Verbose -Message "Load API data for $($resources.Function)"
+    Write-Verbose -Message "Description: $($resources.Description)"
+  
+  }
+
+  Process {
+    
+    $uri = New-URIString -server $Server -endpoint ($resources.URI) -id $id
+    $uri = Test-QueryParam -querykeys ($resources.Query.Keys) -parameters ((Get-Command $function).Parameters.Values) -uri $uri
+    $body = New-BodyString -bodykeys ($resources.Body.Keys) -parameters ((Get-Command $function).Parameters.Values)
+    $result = Submit-Request -uri $uri -header $Header -method $($resources.Method) -body $body
+    $result = Test-ReturnFormat -api $api -result $result -location $resources.Result
+    $result = Set-ObjectTypeName -TypeName $resources.ObjectTName -result $result
+    return $result
+
+  } # End of process
+} # End of function
