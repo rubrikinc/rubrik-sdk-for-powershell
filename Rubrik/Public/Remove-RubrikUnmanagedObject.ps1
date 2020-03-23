d4b073060cbafaf6d12584d8d85cd2eba4874a84
Mike Preston <mwpreston@gmail.com>
2020-03-23 11:38:46 -0400

    added force parameter

--- Rubrik/Public/Remove-RubrikUnmanagedObject.ps1
+++ Rubrik/Public/Remove-RubrikUnmanagedObject.ps1
@@ -1,7 +1,7 @@
 #Requires -Version 3
 function Remove-RubrikUnmanagedObject
 {
-  <#  
+  <#
       .SYNOPSIS
       Removes one or more unmanaged objects known to a Rubrik cluster
 
@@ -40,6 +40,8 @@ function Remove-RubrikUnmanagedObject
     [Parameter(Mandatory = $true,ValueFromPipelineByPropertyName = $true)]
     [Alias('objectType')]
     [String]$Type,
+    # Forces object to be removed regardless of SLA Domain assignment. From Rubrik CDM 5.0 and above, an unmanaged object cannot be deleted if snapshots exist with an SLA Domain assigned to them. The -Force parameter will override this allowing the object, and the snapshots, to be deleted.
+    [Switch]$Force,
     # Rubrik server IP or FQDN
     [String]$Server = $global:RubrikConnection.server,
     # API version
@@ -50,32 +52,45 @@ function Remove-RubrikUnmanagedObject
 
     # The Begin section is used to perform one-time loads of data necessary to carry out the function's purpose
     # If a command needs to be run with each iteration or pipeline input, place it in the Process section
-    
+
     # Check to ensure that a session to the Rubrik cluster exists and load the needed header data for authentication
     Test-RubrikConnection
-    
+
     # API data references the name of the function
     # For convenience, that name is saved here to $function
     $function = $MyInvocation.MyCommand.Name
-        
+
     # Retrieve all of the URI, method, body, query, result, filter, and success details for the API endpoint
     Write-Verbose -Message "Gather API Data for $function"
     $resources = Get-RubrikAPIData -endpoint $function
     Write-Verbose -Message "Load API data for $($resources.Function)"
     Write-Verbose -Message "Description: $($resources.Description)"
-  
+
   }
 
   Process {
 
+    # if force is specified and CDM is > 5.0 remove SLA domain from snapshots first
+    if ($Force -and 5 -ge [float]$rubrikconnection.version.substring(0,3)) {
+      Write-Verbose -Message "Force parameter specified. Removing SLA Domain from existing snapshots"
+      $snapshots = Invoke-RubrikRestCall -api 'internal' -Endpoint "unmanaged_object/$id/snapshot" -Method "GET"
+      Write-Verbose -Message "Assigning UNPROTECTED SLA Domain to snapshot ids: $($snapshots.data.id)"
+      $snapshotarray = New-Object -TypeName System.Collections.Generic.List[String]
+      $snapshots.data.id | ForEach { $snapshotarray.Add($_)}
+      $body = New-Object -TypeName PSObject -Property @{"slaDomainId"="UNPROTECTED";"snapshotIds"=$snapshotarray}
+      Invoke-RubrikRestCall -Endpoint 'unmanaged_object/snapshot/assign_sla' -Method "POST" -api "internal" -Body $body
+    }
+
     $uri = New-URIString -server $Server -endpoint ($resources.URI)
     $uri = Test-QueryParam -querykeys ($resources.Query.Keys) -parameters ((Get-Command $function).Parameters.Values) -uri $uri
-    $body = New-BodyString -bodykeys ($resources.Body.Keys) -parameters ((Get-Command $function).Parameters.Values)        
+    $body = New-BodyString -bodykeys ($resources.Body.Keys) -parameters ((Get-Command $function).Parameters.Values)
     $result = Submit-Request -uri $uri -header $Header -method $($resources.Method) -body $body
     $result = Test-ReturnFormat -api $api -result $result -location $resources.Result
     $result = Test-FilterObject -filter ($resources.Filter) -result $result
 
     return $result
+
+
 
   } # End of process
 } # End of function
