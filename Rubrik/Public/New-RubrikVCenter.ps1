9fdcc863b0a905bca348326c6c3c238f37bfe409
Mike Preston <mwpreston@gmail.com>
2020-03-30 15:07:16 -0400

    New-RubrikVcenter tests and updates

--- Rubrik/Public/New-RubrikVCenter.ps1
+++ Rubrik/Public/New-RubrikVCenter.ps1
@@ -1,22 +1,22 @@
 #Requires -Version 3
 function New-RubrikVCenter
 {
-  <#  
+  <#
       .SYNOPSIS
       Connects to Rubrik and creates new vCenter connection
-            
+
       .DESCRIPTION
       The New-RubrikVCenter cmdlet will  creates new vCenter connection on the system. This does require authentication.
-            
+
       .NOTES
       Adapted by Adam Shuttleworth from scripts by Chris Wahl for community usage
-            
+
       .LINK
       https://rubrik.gitbook.io/rubrik-sdk-for-powershell/command-documentation/reference/new-rubrikvcenter
-            
+
       .EXAMPLE
       New-RubrikVCenter -hostname "test-vcenter.domain.com"
-      This will creates new vCenter connection to "test-vcenter.domain.com" on the current Rubrik cluster  
+      This will creates new vCenter connection to "test-vcenter.domain.com" on the current Rubrik cluster
   #>
 
   [CmdletBinding()]
@@ -24,6 +24,18 @@ function New-RubrikVCenter
     # Hostname (FQDN) of your vCenter Server
     [Parameter(Mandatory=$True)]
     [string]$Hostname,
+    # Username with permissions to connect to the vCenter
+    # Optionally, use the Credential parameter
+    [Parameter(ParameterSetName='UserPassword',Mandatory=$true)]
+    [String]$Username,
+    # Password for the Username provided
+    # Optionally, use the Credential parameter
+    [Parameter(ParameterSetName='UserPassword',Mandatory=$true)]
+    [SecureString]$Password,
+    # Credentials with permission to connect to the vCenter
+    # Optionally, use the Username and Password parameters
+    [Parameter(ParameterSetName='Credential',Mandatory=$true)]
+    [System.Management.Automation.CredentialAttribute()] $Credential,
     # Rubrik server IP or FQDN
     [String]$Server = $global:RubrikConnection.server,
     # ID of the Rubrik cluster or me for self
@@ -37,23 +49,29 @@ function New-RubrikVCenter
 
     # The Begin section is used to perform one-time loads of data necessary to carry out the function's purpose
     # If a command needs to be run with each iteration or pipeline input, place it in the Process section
-    $Credentials=$(Get-Credential -Message "Type vCenter Credentials.")
-    $username = $Credentials.UserName
-    $password = $Credentials.GetNetworkCredential().Password
+
+    if ($Credential) {
+      $usernameforbody = $Credential.UserName
+      $passwordforbody = $Credential.GetNetworkCredential().Password
+    }
+    elseif ($Username) {
+      $usernameforbody = $Username
+      $passwordforbody = ConvertFrom-SecureString -SecureString $password -AsPlainText
+    }
 
     # Check to ensure that a session to the Rubrik cluster exists and load the needed header data for authentication
     Test-RubrikConnection
-    
+
     # API data references the name of the function
     # For convenience, that name is saved here to $function
     $function = $MyInvocation.MyCommand.Name
-        
+
     # Retrieve all of the URI, method, body, query, result, filter, and success details for the API endpoint
     Write-Verbose -Message "Gather API Data for $function"
     $resources = Get-RubrikAPIData -endpoint $function
     Write-Verbose -Message "Load API data for $($resources.Function)"
     Write-Verbose -Message "Description: $($resources.Description)"
-  
+
   }
 
   Process {
@@ -65,14 +83,14 @@ function New-RubrikVCenter
 
     $body = @{
         $resources.Body.hostname = $hostname
-        $resources.Body.username = $username
-        $resources.Body.password = $password
+        $resources.Body.username = $usernameforbody
+        $resources.Body.password = $passwordforbody
     }
-        
+
     $body = ConvertTo-Json $body
     Write-Verbose -Message "Body = $body"
     #endregion
-    
+
     $uri = New-URIString -server $Server -endpoint ($resources.URI) -id $id
     #$uri = Test-QueryParam -querykeys ($resources.Query.Keys) -parameters ((Get-Command $function).Parameters.Values) -uri $uri
     #$body = New-BodyString -bodykeys ($resources.Body.Keys) -parameters ((Get-Command $function).Parameters.Values)
