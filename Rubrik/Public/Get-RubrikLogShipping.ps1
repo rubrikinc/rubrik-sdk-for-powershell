9901cd4ab7ba9ca1df3b48202d63ded4c5fa8fcf
Mike Preston <mwpreston@gmail.com>
2020-04-06 14:34:53 -0400

    added position to PrimaryDBName on LogShipping

--- Rubrik/Public/Get-RubrikLogShipping.ps1
+++ Rubrik/Public/Get-RubrikLogShipping.ps1
@@ -1,7 +1,7 @@
 #requires -Version 3
 function Get-RubrikLogShipping
 {
-  <#  
+  <#
       .SYNOPSIS
       Retrieves all log shipping configuration objects. Results can be filtered and sorted.
 
@@ -42,29 +42,31 @@ function Get-RubrikLogShipping
   Param(
     [Parameter(ValueFromPipelineByPropertyName = $true)]
     [String]$id,
-       
+
     [Alias('primary_database_id')]
     [String]$PrimaryDatabaseId,
-    
+
+    [Parameter(
+      Position = 0)]
     [Alias('primary_database_name')]
     [String]$PrimaryDatabaseName,
-    
+
     [Alias('secondary_database_name')]
     [String]$SecondaryDatabaseName,
-    
+
     #Log Shipping Target Server
     [String]$location,
-    
+
     [ValidateSet("OK", "Broken", "Initializing", "Stale")]
     [String]$status,
-    
+
     [String]$limit,
-    
+
     [String]$offset,
 
     [ValidateSet("secondaryDatabaseName", "primaryDatabaseName", "lastAppliedPoint", "location")]
     [String]$sort_by,
-    
+
     [ValidateSet("asc", "desc")]
     [String]$sort_order,
 
@@ -73,7 +75,7 @@ function Get-RubrikLogShipping
 
     # Rubrik server IP or FQDN
     [String]$Server = $global:RubrikConnection.server,
-    
+
     # API version
     [String]$api = $global:RubrikConnection.api
   )
@@ -82,20 +84,20 @@ function Get-RubrikLogShipping
 
     # The Begin section is used to perform one-time loads of data necessary to carry out the function's purpose
     # If a command needs to be run with each iteration or pipeline input, place it in the Process section
-    
+
     # Check to ensure that a session to the Rubrik cluster exists and load the needed header data for authentication
     Test-RubrikConnection
-    
+
     # API data references the name of the function
     # For convenience, that name is saved here to $function
     $function = $MyInvocation.MyCommand.Name
-        
+
     # Retrieve all of the URI, method, body, query, result, filter, and success details for the API endpoint
     Write-Verbose -Message "Gather API Data for $function"
     $resources = Get-RubrikAPIData -endpoint $function
     Write-Verbose -Message "Load API data for $($resources.Function)"
     Write-Verbose -Message "Description: $($resources.Description)"
-  
+
   }
 
   Process {
