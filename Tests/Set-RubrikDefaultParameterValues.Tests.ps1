90e88171ac8d3e59acf95aa372822b92219dc9d6
Mike Preston <mwpreston@gmail.com>
2020-04-13 13:06:43 -0400

    unit test for Set-RubrikDefaultParameterValues

--- Tests/Set-RubrikDefaultParameterValues.Tests.ps1
+++ Tests/Set-RubrikDefaultParameterValues.Tests.ps1
@@ -1,1 +1,38 @@
-﻿
+﻿Remove-Module -Name 'Rubrik' -ErrorAction 'SilentlyContinue'
+Import-Module -Name './Rubrik/Rubrik.psd1' -Force
+
+foreach ( $privateFunctionFilePath in ( Get-ChildItem -Path './Rubrik/Private' | Where-Object extension -eq '.ps1').FullName  ) {
+    . $privateFunctionFilePath
+}
+
+Describe -Name 'Private/Set-RubrikDefaultParameterValues.Tests.ps1' -Tag 'Private', 'Set-RubrikDefaultParameterValues' -Fixture {
+    #region init
+    $global:rubrikConnection = @{
+        id      = 'test-id'
+        userId  = 'test-userId'
+        token   = 'test-token'
+        server  = 'test-server'
+        header  = @{ 'Authorization' = 'Bearer test-authorization' }
+        time    = (Get-Date)
+        api     = 'v1'
+        version = '4.0.5'
+    }
+    $global:rubrikOptions = @{
+        DefaultParameterValue = @{
+            PrimaryClusterId = 'local'
+        }
+        ModuleOption = @{
+            ApplyCustomViewDefinitions = 'True'
+            CredentialPath = ''
+        }
+    }
+    #endregion
+    Context -Name 'Defaults are applied' -Fixture {
+        It -Name "PrimaryClusterId should exist and be local" -Test {
+            Set-RubrikDefaultParameterValues
+            $Global:PSDefaultParameterValues."*Rubrik*:PrimaryClusterId" |
+                Should be "local"
+        }
+    }
+
+}
