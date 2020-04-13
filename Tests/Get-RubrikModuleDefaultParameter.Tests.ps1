14e0808681cae1a75c6beb42ff4be1579ed41d71
Mike Preston <mwpreston@gmail.com>
2020-04-13 13:34:32 -0400

    more tests

---
+++ Tests/Get-RubrikModuleDefaultParameter.Tests.ps1
@@ -0,0 +1,42 @@
+﻿Remove-Module -Name 'Rubrik' -ErrorAction 'SilentlyContinue'
+Import-Module -Name './Rubrik/Rubrik.psd1' -Force
+
+foreach ( $privateFunctionFilePath in ( Get-ChildItem -Path './Rubrik/Private' | Where-Object extension -eq '.ps1').FullName  ) {
+    . $privateFunctionFilePath
+}
+
+Describe -Name 'Public/Get-RubrikModuleDefaultParameter' -Tag 'Public', 'Get-RubrikModuleDefaultParameter' -Fixture {
+    #region init
+    $global:rubrikConnection = @{
+        id      = 'test-id'
+        userId  = 'test-userId'
+        token   = 'test-token'
+        server  = 'test-server'
+        header  = @{ 'Authorization' = 'Bearer test-authorization' }
+        time    = (Get-Date)
+        api     = 'v1'
+        version = '4.0.5'
+    }
+    $global:rubrikOptions = @{
+        DefaultParameterValue = @{
+            PrimaryClusterId = 'local'
+            AnotherParameter = 'Value'
+        }
+        ModuleOption = @{
+            ApplyCustomViewDefinitions = 'True'
+            CredentialPath = ''
+        }
+    }
+    #endregion
+
+    Context -Name 'Results Filtering' {
+        It -Name "Should return count of 2" -Test {
+            (Get-RubrikModuleDefaultParameter).Count |
+                Should -BeExactly 2
+        }
+        It -Name "Should return local for primaryclusterid" -Test {
+            (Get-RubrikModuleDefaultParameter -ParameterName "PrimaryClusterId").primaryclusterid |
+                Should -BeExactly "local"
+        }
+    }
+}
---
+++ Tests/Get-RubrikModuleOption.Tests.ps1
@@ -0,0 +1,42 @@
+﻿Remove-Module -Name 'Rubrik' -ErrorAction 'SilentlyContinue'
+Import-Module -Name './Rubrik/Rubrik.psd1' -Force
+
+foreach ( $privateFunctionFilePath in ( Get-ChildItem -Path './Rubrik/Private' | Where-Object extension -eq '.ps1').FullName  ) {
+    . $privateFunctionFilePath
+}
+
+Describe -Name 'Public/Get-RubrikModuleOption' -Tag 'Public', 'Get-RubrikModuleOption' -Fixture {
+    #region init
+    $global:rubrikConnection = @{
+        id      = 'test-id'
+        userId  = 'test-userId'
+        token   = 'test-token'
+        server  = 'test-server'
+        header  = @{ 'Authorization' = 'Bearer test-authorization' }
+        time    = (Get-Date)
+        api     = 'v1'
+        version = '4.0.5'
+    }
+    $global:rubrikOptions = @{
+        DefaultParameterValue = @{
+            PrimaryClusterId = 'local'
+            AnotherParameter = 'Value'
+        }
+        ModuleOption = @{
+            ApplyCustomViewDefinitions = 'True'
+            CredentialPath = 'c:\\credentialpath\\creds.xml'
+        }
+    }
+    #endregion
+
+    Context -Name 'Results Filtering' {
+        It -Name "Should return count of 2" -Test {
+            (Get-RubrikModuleOption).Count |
+                Should -BeExactly 2
+        }
+        It -Name "Should return true for ApplyCustomViewDefinitions" -Test {
+            (Get-RubrikModuleOption -OptionName "ApplyCustomViewDefinitions").ApplyCustomViewDefinitions |
+                Should -BeExactly "True"
+        }
+    }
+}
