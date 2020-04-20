b8f7bf4f024926c67b34b7d9bffb0fbe1f23cf11
Mike Preston <mwpreston@gmail.com>
2020-04-20 12:19:12 -0400

    unit test

---
+++ Tests/Get-RubrikDetailedResults.Tests.ps1
@@ -0,0 +1,42 @@
+﻿Remove-Module -Name 'Rubrik' -ErrorAction 'SilentlyContinue'
+Import-Module -Name './Rubrik/Rubrik.psd1' -Force
+
+foreach ( $privateFunctionFilePath in ( Get-ChildItem -Path './Rubrik/Private' | Where-Object extension -eq '.ps1').FullName  ) {
+    . $privateFunctionFilePath
+}
+
+Describe -Name 'Private/Get-RubrikDetailedResults' -Tag 'Private', 'Get-RubrikDetailedResults' -Fixture {
+    #region init
+    $global:rubrikConnection = @{
+        id      = 'test-id'
+        userId  = 'test-userId'
+        token   = 'test-token'
+        server  = 'test-server'
+        header  = @{ 'Authorization' = 'Bearer test-authorization' }
+        time    = (Get-Date)
+        api     = 'v1'
+        version = '4.0'
+    }
+
+
+    Context -Name "Test Returned Data" -Fixture {
+        Mock -CommandName Test-RubrikConnection -Verifiable -ModuleName 'Rubrik' -MockWith {}
+        Mock -CommandName Submit-Request -Verifiable -ModuleName 'Rubrik' -MockWith {
+            @{
+                'name'                   = 'test-vm1'
+                'id'                     = 'vm1'
+                'effectiveSlaDomainName' = 'test-valid_sla_name'
+            }
+        }
+        it -Name "Returns proper data" -Test {
+            $results = @{
+                'name'  = 'test-vm1'
+                'id'    = 'vm1'
+            }
+            $arrResults = @()
+            $arrResults += $results
+            (Get-RubrikDetailedResults -result $arrResults -cmdlet 'Get-RubrikVM').id |
+                Should -Be 'vm1'
+        }
+    }
+}
