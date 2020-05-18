060e3e0dbbd9f9178581201c2d169677f972ca5d
Jaap Brasser <noreply@jaapbrasser.com>
2020-05-18 11:28:49 +0200

    Added -id validation to UriString 🧵

--- Rubrik/Private/New-URIString.ps1
+++ Rubrik/Private/New-URIString.ps1
@@ -17,6 +17,11 @@
     An id value to be planted into the path or optionally at the end of the URI to retrieve a single object
   #>
 
+  # Validation of id param
+  if (($id.Length -gt 100) -or ($id -match '^@\{')) {
+    Write-Error -Message "Please validate ID input, invalid ID provided: '$id'" -ErrorAction Stop
+  }
+
   Write-Verbose -Message 'Build the URI'
   # If we find {id} in the path, replace it with the $id value  
   if ($endpoint.Contains('{id}')) {
