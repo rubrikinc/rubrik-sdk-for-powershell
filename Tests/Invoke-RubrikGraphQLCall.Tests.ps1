4f75499e4cbd0b5b038c81270cf342d037fec093
Jaap Brasser <noreply@jaapbrasser.com>
2020-05-25 16:19:27 +0200

    Created unit tests for Invoke-RubrikGraphQLCall 🥼

--- Tests/Invoke-RubrikGraphQLCall.Tests.ps1
+++ Tests/Invoke-RubrikGraphQLCall.Tests.ps1
@@ -5,7 +5,7 @@ foreach ( $privateFunctionFilePath in ( Get-ChildItem -Path './Rubrik/Private' |
     . $privateFunctionFilePath
 }
 
-Describe -Name 'Public/Invoke-RubrikRestCall' -Tag 'Public', 'Invoke-RubrikRestCall' -Fixture {
+Describe -Name 'Public/Invoke-RubrikGraphQLCall' -Tag 'Public', 'Invoke-RubrikGraphQLCall' -Fixture {
     #region init
     $global:rubrikConnection = @{
         id      = 'test-id'
@@ -21,51 +21,80 @@ Describe -Name 'Public/Invoke-RubrikRestCall' -Tag 'Public', 'Invoke-RubrikRestC
 
     Context -Name 'Results Filtering' {
         Mock -CommandName Test-RubrikConnection -Verifiable -ModuleName 'Rubrik' -MockWith {}
+        Mock -CommandName Invoke-WebRequest -ModuleName 'Rubrik' -MockWith {
+            [pscustomobject]@{
+                'Content' = '{"Name":"VM3","ID":"VirtualMachine:33333","powerStatus":"poweredOff"}'
+            }
+        }
         Mock -CommandName Submit-Request -Verifiable -ModuleName 'Rubrik' -MockWith {
-            @{ 
-                'total' = '3'
-                'data'  =   @{ 
-                                'Name'        = 'VM1'
-                                'ID'          = 'VirtualMachine:11111'
-                                'powerStatus' = 'poweredOn'
-                            },
-                            @{ 
-                                'Name'        = 'VM2'
-                                'ID'          = 'VirtualMachine:22222'
-                                'powerStatus' = 'poweredOn'
-                            },
-                            @{ 
-                                'Name'        = 'VM3'
-                                'ID'          = 'VirtualMachine:33333'
-                                'powerStatus' = 'poweredOff'
+            [pscustomobject]@{ 
+                'data'  =   [pscustomobject]@{
+                            'Nodes' =[pscustomobject]@{ 
+                                        'Name'        = 'VM1'
+                                        'ID'          = 'VirtualMachine:11111'
+                                        'powerStatus' = 'poweredOn'
+                                    },
+                                    [pscustomobject]@{ 
+                                        'Name'        = 'VM2'
+                                        'ID'          = 'VirtualMachine:22222'
+                                        'powerStatus' = 'poweredOn'
+                                    },
+                                    [pscustomobject]@{ 
+                                        'Name'        = 'VM3'
+                                        'ID'          = 'VirtualMachine:33333'
+                                        'powerStatus' = 'poweredOff'
+                                    }
                             }
             }
         }
 
-        It -Name 'Requesting all should return total of 3' -Test {
-            ( Invoke-RubrikRestCall -Endpoint 'vmware/vm' -Method "Get"   ).Total |
-                Should -BeExactly '3'
+
+        It -Name 'Request with body should return a single object' -Test {
+            @( Invoke-RubrikGraphQLCall -Body 'query').count |
+                Should -BeExactly 1
         }
+
+        It -Name 'Request should be a PowerShell custom object' -Test {
+            Invoke-RubrikGraphQLCall -Body 'query' |
+                Should -BeOfType 'PSCustomObject'
+        }
+
+        It -Name 'Request with -ReturnNode should return an array of 3 objects' -Test {
+            ( Invoke-RubrikGraphQLCall -ReturnNode -Body 'query').count |
+                Should -BeExactly 3
+        }
+
+        It -Name 'Request with -ReturnJSON should be a string' -Test {
+            ( Invoke-RubrikGraphQLCall -ReturnJSON -Body 'query') |
+                Should -BeOfType 'String'
+        }
+
+        It -Name 'Request with -ReturnJSON should be the correct string and content should expand' -Test {
+            ( Invoke-RubrikGraphQLCall -ReturnJSON -Body 'query') |
+                Should -BeExactly '{"Name":"VM3","ID":"VirtualMachine:33333","powerStatus":"poweredOff"}'
+        }
+        
         Assert-VerifiableMock
-        Assert-MockCalled -CommandName Test-RubrikConnection -ModuleName 'Rubrik' -Times 1
-        Assert-MockCalled -CommandName Submit-Request -ModuleName 'Rubrik' -Times 1
+        Assert-MockCalled -CommandName Test-RubrikConnection -ModuleName 'Rubrik' -Exactly 5
+        Assert-MockCalled -CommandName Submit-Request -ModuleName 'Rubrik' -Exactly 3
+        Assert-MockCalled -CommandName Invoke-WebRequest -ModuleName 'Rubrik' -Exactly 2
     }
     Context -Name 'Parameter Validation' { 
-        It -Name 'Endpoint cannot be null or empty' -Test {
-            { Invoke-RubrikRestCall -Endpoint $null -Method "GET" } |
-                Should -Throw "Cannot validate argument on parameter 'Endpoint'. The argument is null or empty. Provide an argument that is not null or empty, and then try the command again."
-        }
-        It -Name 'ValidateSet - Method' -Test {
-            { Invoke-RubrikRestCall -Endpoint 'vmware/vm' -Method "non-existant" } |
-                Should -Throw "Cannot validate argument on parameter 'Method'."
-        }  
-        It -Name 'Query cannot be null or empty' -Test {
-            { Invoke-RubrikRestCall -Endpoint 'vmware/vm' -Method "POST" -Query $null } |
-                Should -Throw "Cannot validate argument on parameter 'Query'."
-        } 
         It -Name 'Body cannot be null or empty' -Test {
-            { Invoke-RubrikRestCall -Endpoint 'vmware/vm' -Method "POST" -Body $null } |
+            { Invoke-RubrikGraphQLCall -Body $null } |
+                Should -Throw "Cannot validate argument on parameter 'Body'."
+        }
+        It -Name 'Body cannot be null or empty - JSON param set' -Test {
+            { Invoke-RubrikGraphQLCall -ReturnJSON -Body $null} |
                 Should -Throw "Cannot validate argument on parameter 'Body'."
         }        
+        It -Name 'Body cannot be null or empty - Node param set' -Test {
+            { Invoke-RubrikGraphQLCall -ReturnNode -Body $null} |
+                Should -Throw "Cannot validate argument on parameter 'Body'."
+        }        
+        It -Name 'Cannot specify same param twice' -Test {
+            { Invoke-RubrikGraphQLCall -ReturnJSON -ReturnJSON -Body 'query' } |
+                Should -Throw "Cannot bind parameter because parameter 'ReturnJSON' is specified more than once. To provide multiple values to parameters that can accept multiple values, use the array syntax. For example, ""-parameter value1,value2,value3""."
+        }  
     }
 }
