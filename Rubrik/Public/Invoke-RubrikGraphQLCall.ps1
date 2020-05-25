565e63e3f84c0990d67b855e1457ba51284061cb
Jaap Brasser <noreply@jaapbrasser.com>
2020-05-25 12:40:42 +0200

    Added documentation and initial cmdlet logic 🖥

--- Rubrik/Public/Invoke-RubrikGraphQLCall.ps1
+++ Rubrik/Public/Invoke-RubrikGraphQLCall.ps1
@@ -1,15 +1,10 @@
 function Invoke-RubrikGraphQLCall {
 <#
 .SYNOPSIS
-Provides generic interface to make Rubrik REST API calls
+Provides generic interface to make Rubrik GraphQL API calls
 
 .DESCRIPTION
-The Invoke-RubrikGraphQLCall allows users to make raw API endpoint calls to the Rubrik REST interface. The user
-will need to manage the format of both the endpoint call(including resource ids) and body, but provides the
-option to make cmdlet independent API calls for automating Rubrik actions through PowerShell. The Rubrik API
-reference is found on the Rubrik device at:
-<Rubrik IP>/docs/v1
-<Rubrik IP>/docs/v1/playground
+The Invoke-RubrikGraphQLCall allows users to make raw API endpoint calls to the Rubrik GraphQL interface. To find out more about the GraphQL endpoint take a look at the Rubrik GraphQL Playground project that is hosted on GitHub: https://github.com/rubrikinc/graphql-playground
 
 .NOTES
 Written by Jaap Brasser for community usage
@@ -20,88 +15,100 @@ GitHub: jaapbrasser
 https://rubrik.gitbook.io/rubrik-sdk-for-powershell/command-documentation/reference/invoke-rubrikgraphqlcall
 
 .EXAMPLE
-Invoke-RubrikGraphQLCall -Endpoint 'vmware/vm' -Method GET
+Invoke-RubrikGraphQLCall -Body '{"query":"{vmwareVcenterTotalCount}"}' -Verbose
 
-Retrieve the raw output for all VMWare VMs being managed by the Rubrik device.
-
-.EXAMPLE
-Invoke-RubrikGraphQLCall -Endpoint 'vmware/vm' -Method GET -Query @{'name'='msf-sql2016'}
-Retrieve the raw output for the VMWare VM msf-sql2016 using a query parameter.
+Will retrieve the total count of VMware vCenters connected to the Rubrik cluster as a PowerShell object
 
 .EXAMPLE
-$body = New-Object -TypeName PSObject -Property @{'slaID'='INHERIT';'ForceFullSnapshot'='FALSE'}
-Invoke-RubrikGraphQLCall -Endpoint 'vmware/vm/VirtualMachine:::fbcb1f51-9520-4227-a68c-6fe145982f48-vm-649/snapshot' -Method POST -Body $body
+Invoke-RubrikGraphQLCall -ReturnJSON -Body '{"query":"query{vms {\nnumProtected\n  numUnprotected\n}}"}'
 
-Execute an on-demand snapshot for the VMWare VM where the id is part of the endpoint.
+Returns the number of protected and unprotected VMware VM objects as a JSON string
 
 .EXAMPLE
-$body = New-Object -TypeName PSObject -Property @{'isPassthrough'=$true;'shareId'='HostShare:::11111';'templateId'='FilesetTemplate:::22222'}
-Invoke-RubrikGraphQLCall -Endpoint 'fileset_template/bulk' -Method POST -Body $body -BodyAsArray
+Invoke-RubrikGraphQLCall -ReturnNode -Body '{"query":"query{\r\n  vmwareVirtualMachineConnection{\nedges {\n  node {\nhostId\neffectiveSlaDomain {\nid\nname\n}\nprimaryClusterId\nconfiguredSlaDomainId\nid\nvmwareToolsInstalled\nisRelic\nname\nvcenterId\nfolderPath {\nid\nname\n}\ninfraPath {\nid\nname\n}\nagentStatus {\nagentStatus\ndisconnectReason\n}\nhostName\nclusterName\n  }\n}\r\n  }\r\n}","variables":{}}'
 
-Creates a new fileset from the given fileset template and the given host id supporting Direct Archive.  Since fileset_template/bulk expects an array, we force the single item array with the BodyAsArray parameter.
+Returns all VMware VMs on the Rubrik Cluster and displays the individual objects by using the -ReturnNode parameter
+
+.EXAMPLE
+Invoke-RubrikGraphQLCall -ReturnNode -Body '{"query":"query OrganizationSummary(\n $name: String,\n $isGlobal: Boolean,\n $sortBy: String,\n $sortOrder: String,\n $first: Int,\n $after: String,\n) {\n organizationConnection(\n name: $name,\n isGlobal: $isGlobal,\n sortBy: $sortBy,\n sortOrder: $sortOrder,\n first: $first,\n after: $after\n ) {\n nodes {\n id\n name\n isGlobal\n exclusivenessLevel\n admins {\n id\n name\n }\n envoyStatus\n }\n pageInfo {\n endCursor\n hasNextPage\n }\n }\n}\n","variables":{"sortBy":"name","sortOrder":"asc","first":3,"name":"org","isGlobal":false}}'
+
+Returns informations on Rubrik Organizations, filtered by name in ascending order and only including organizations with 'org' in their name property. Returns the individual organization objects using the -ReturnNode parameter
 #>
     
-      [cmdletbinding()]
-      Param (
-          #Rubrik API endpoint, DO NOT USE LEADING '/'
-          [Parameter(Mandatory = $true,HelpMessage = 'REST Endpoint')]
-          [ValidateNotNullorEmpty()]
-          [System.String]$Endpoint,
-          #Hash table body to pass to API call
-          [Parameter(Mandatory = $false,HelpMessage = 'REST Content')]
-          [ValidateNotNullorEmpty()]
-          [psobject]$Body,
-          #Force the body as an array (For endpoints requiring single item arrays)
-          [Parameter(Mandatory = $false, HelpMessage = 'Force Body to be an array')]
-          [Switch]$BodyAsArray,
-          # Rubrik server IP or FQDN
-          [String]$Server = $global:RubrikConnection.server,
-          # API version
-          [ValidateNotNullorEmpty()]
-          [String]$api = $global:RubrikConnection.api
-      )
-      BEGIN
-      {
+    [cmdletbinding()]
+    Param (
+        #Hash table body to pass to API call
+        [Parameter(Mandatory = $false,HelpMessage = 'REST Content')]
+        [ValidateNotNullorEmpty()]
+        [string]$Body,
+        [switch]$ReturnJSON,
+        [switch]$ReturnNode,
+        # Rubrik server IP or FQDN
+        [String]$Server = $global:RubrikConnection.server,
+        # API version
+        [ValidateNotNullorEmpty()]
+        [String]$api = $global:RubrikConnection.api
+    )
+
+    begin {
         #connect to Rubrik if not already connected
         Test-RubrikConnection
-      }
-    
-      PROCESS
-      {
+    }
+
+    process {
         #execute GraphQL operation
         try {
-    
+
             #construct uri
             [string]$uri = New-URIString -server $Server -endpoint "/api/internal/graphql"
-    
-            #If query object, add query parameters to URI
-            if($query)
-            {
-                $querystring = @()
-                foreach($q in $query.Keys)
-                {
-                    $querystring += "$q=$($query[$q])"
+            Write-Verbose -Message "Body = $Body"
+            
+            if ($ReturnJSON) {
+                $WebSplat = @{
+                    Uri = $uri
+                    Headers = $Header
+                    ContentType = 'application/json'
+                    Body = $Body
+                    Method = 'POST'
+                    UseBasicParsing = $true
                 }
-                $uri = New-QueryString -query $querystring -uri $uri
+
+                if (Test-PowerShellSix) {
+                    if ($rubrikOptions.ModuleOption.DefaultWebRequestTimeOut) {
+                        $result = Invoke-WebRequest -SkipCertificateCheck -TimeoutSec $rubrikOptions.ModuleOption.DefaultWebRequestTimeOut @WebSplat |
+                            Select-Object -ExpandProperty Content
+                    } else {
+                        $result = Invoke-WebRequest -SkipCertificateCheck @WebSplat |
+                            Select-Object -ExpandProperty Content
+                    }
+                } else {
+                    if ($rubrikOptions.ModuleOption.DefaultWebRequestTimeOut) {
+                        $result = Invoke-WebRequest -TimeoutSec $rubrikOptions.ModuleOption.DefaultWebRequestTimeOut @WebSplat |
+                            Select-Object -ExpandProperty Content
+                    } else {
+                        $result = Invoke-WebRequest @WebSplat 
+                            Select-Object -ExpandProperty Content
+                    }
+                }
+            } elseif ($ReturnNode) {
+                $result = Submit-Request -uri $uri -header $Header -method POST -body $Body |
+                    Select-Object -ExpandProperty data
+                if (($newresult = $result.$($result.psobject.properties.name).nodes)) {
+                    $result = $newresult
+                } elseif (($newresult = $result.$($result.psobject.properties.name).edges.node)) {
+                    $result = $newresult
+                } else {
+                    Write-Verbose -Message "Nodes property not found in $($result.psobject.properties.name), either no results or results are nested."
+                }
+            } else {
+                $result = Submit-Request -uri $uri -header $Header -method POST -body $Body |
+                    Select-Object -ExpandProperty data
             }
-    
-            #If Method is not a GET call and a REST Body is passed, build the JSON body
-            if($Method -ne 'GET' -and $body){
-                if ($BodyAsArray) {
-                    [string]$JsonBody = ConvertTo-Json -inputobject @($Body) -Depth 10
-                }
-                else {
-                    [string]$JsonBody = $Body | ConvertTo-Json -Depth 10
-                }
-            }
-            Write-Verbose "URI string: $uri"
-            Write-Verbose "Body string: $JsonBody"
-            $result = Submit-Request -uri $uri -header $Header -method $Method -body $JsonBody
         }
         catch {
             throw $_
         }
-    
+
         return $result
-      }
     }
+}
