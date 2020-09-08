37ef0c9a64bb2b6d910d180332e0fcc8db506071
Jaap Brasser <noreply@jaapbrasser.com>
2020-09-08 11:57:10 +0200

    Restored Remove-RubrikOrgAu functionality for older CDM version 🗝

--- Rubrik/Public/Remove-RubrikOrgAuthorization.ps1
+++ Rubrik/Public/Remove-RubrikOrgAuthorization.ps1
@@ -118,7 +118,7 @@ function Remove-RubrikOrgAuthorization
       $uri = New-URIString -server $Server -endpoint ($resources.URI)
       $uri = Test-QueryParam -querykeys ($resources.Query.Keys) -parameters ((Get-Command $function).Parameters.Values) -uri $uri
       #$body = New-BodyString -bodykeys ($resources.Body.Keys) -parameters ((Get-Command $function).Parameters.Values)
-      #$result = Submit-Request -uri $uri -header $Header -method $($resources.Method) -body $body
+      $result = Submit-Request -uri $uri -header $Header -method $($resources.Method) -body $body
       $result = Test-ReturnFormat -api $api -result $result -location $resources.Result
       $result = Test-FilterObject -filter ($resources.Filter) -result $result
       $result = Set-ObjectTypeName -TypeName $resources.ObjectTName -result $result
