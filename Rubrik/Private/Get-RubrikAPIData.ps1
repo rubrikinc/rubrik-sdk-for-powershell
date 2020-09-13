029a915cdbcb9424795e2ee6da937fbf62ef5b2d
Jaap Brasser <noreply@jaapbrasser.com>
2020-09-13 07:20:33 +0200

    Updated APIData for #689

--- Rubrik/Private/Get-RubrikAPIData.ps1
+++ Rubrik/Private/Get-RubrikAPIData.ps1
@@ -2317,6 +2317,21 @@ function Get-RubrikAPIData {
                 Success     = '200'
                 ObjectTName = 'Rubrik.VMwareVM'
             }
+            '5.2' = @{
+                Description = 'Update a VM with the specified SLA Domain.'
+                URI         = '/api/v2/sla_domain/{id}/assign'
+                Method      = 'Patch'
+                Body        = @{
+                    managedIds = [System.Collections.ArrayList]@()
+                    configuredSlaDomainId     = 'configuredSlaDomainId'
+                    existingSnapshotRetention = 'existingSnapshotRetention'
+                }
+                Query       = ''
+                Result      = ''
+                Filter      = ''
+                Success     = '200'
+                ObjectTName = 'Rubrik.VMwareVM'
+            }
         }
         'Protect-RubrikVolumeGroup'             = @{
             '1.0' = @{
