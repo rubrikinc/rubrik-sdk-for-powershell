c60f0d22aecc98990bc324864078dbbd4f97f920
Jaap Brasser <noreply@jaapbrasser.com>
2021-04-08 22:02:13 +0200

    Updated tests to do something ™ #733

--- Tests/Test-RubrikSnapshotVerification.Tests.ps1
+++ Tests/Test-RubrikSnapshotVerification.Tests.ps1
@@ -5,7 +5,7 @@ foreach ( $privateFunctionFilePath in ( Get-ChildItem -Path './Rubrik/Private' |
     . $privateFunctionFilePath
 }
 
-Describe -Name 'Public/Start-RubrikManagedVolumeSnapshot' -Tag 'Public', 'Start-RubrikManagedVolumeSnapshot' -Fixture {
+Describe -Name 'Public/Test-RubrikSnapshotVerification' -Tag 'Public', 'Test-RubrikSnapshotVerification' -Fixture {
     #region init
     $global:rubrikConnection = @{
         id      = 'test-id'
@@ -15,7 +15,7 @@ Describe -Name 'Public/Start-RubrikManagedVolumeSnapshot' -Tag 'Public', 'Start-
         header  = @{ 'Authorization' = 'Bearer test-authorization' }
         time    = (Get-Date)
         api     = 'v1'
-        version = '4.0.5'
+        version = '5.3'
     }
     #endregion
  
@@ -23,12 +23,18 @@ Describe -Name 'Public/Start-RubrikManagedVolumeSnapshot' -Tag 'Public', 'Start-
         Mock -CommandName Test-RubrikConnection -Verifiable -ModuleName 'Rubrik' -MockWith {}
         Mock -CommandName Submit-Request -Verifiable -ModuleName 'Rubrik' -MockWith {
             @{
-                'snapshotId' = '11111a'
+                'id' = 'BACKUP_INTEGRITY_VERIFICATION_eedf871d-65d6-4595-8e25-50ff79a2cefc_d0376dc4-af94-494a-b4ea-5140812f5eb5:::0'
+                'status' = 'RUNNING'
+                'progress' = '0'
+                'startTime' = '4/8/2021 5:13:17 PM'
+                'nodeId' = 'cluster:::22222'
+                'links' = 'http:; rel=self}}'
+                'snapshotVerificationInfo' = '@{snapshotId=b33a8'
             }
         }
-        It -Name 'Request Fulfilled' -Test {
-            (Start-RubrikManagedVolumeSnapshot -id 'ManagedVolume:::11111' ).snapshotId |
-                Should -BeExactly '11111a'
+        It -Name 'Request Initiated' -Test {
+            (Test-RubrikSnapshotVerification -id 'VirtualMachine:::11111' ).snapshotVerificationInfo |
+                Should -BeExactly '@{snapshotId=b33a8'
         }
 
         It -Name 'Parameter ID must be present' -Test {
@@ -37,7 +43,7 @@ Describe -Name 'Public/Start-RubrikManagedVolumeSnapshot' -Tag 'Public', 'Start-
         }
 
         Assert-VerifiableMock
-        Assert-MockCalled -CommandName Test-RubrikConnection -ModuleName 'Rubrik' -Times 1
-        Assert-MockCalled -CommandName Submit-Request -ModuleName 'Rubrik'  -Times 1
+        Assert-MockCalled -CommandName Test-RubrikConnection -ModuleName 'Rubrik' -Exactly 1
+        Assert-MockCalled -CommandName Submit-Request -ModuleName 'Rubrik'  -Exactly 1
     }
 }
