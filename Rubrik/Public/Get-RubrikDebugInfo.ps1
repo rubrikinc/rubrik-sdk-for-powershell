b42159e5eaf75a1c6be89e1fbf1908dbac616055
Mike Preston <mwpreston@gmail.com>
2021-04-09 09:09:32 -0400

    added module options and parameters

--- Rubrik/Public/Get-RubrikDebugInfo.ps1
+++ Rubrik/Public/Get-RubrikDebugInfo.ps1
@@ -1,21 +1,21 @@
 #Requires -Version 3
 function Get-RubrikDebugInfo
 {
-  <#  
+  <#
     .SYNOPSIS
-    Displays information about the Rubrik module and the PowerShell 
-        
+    Displays information about the Rubrik module and the PowerShell
+
     .DESCRIPTION
     The Get-RubrikDebugInfo cmdlet will retrieve the version of code that is actively running on the system. It will gather essential information that can be used for quickly troubleshooting issues
-        
+
     .NOTES
     Written by Jaap Brasser for community usage
     Twitter: @jaap_brasser
     GitHub: JaapBrasser
-        
+
     .LINK
     https://rubrik.gitbook.io/rubrik-sdk-for-powershell/command-documentation/reference/get-rubrikdebuginfo
-        
+
     .EXAMPLE
     Get-RubrikDebugInfo
 
@@ -54,6 +54,10 @@ function Get-RubrikDebugInfo
     # Add module information
     $HashProps.RubrikCurrentModuleVersion = (Get-Module -Name Rubrik) | ForEach-Object {'{0}-{1}' -f $_.Version,$_.PrivateData.PSData.Prerelease}
     $HashProps.RubrikInstalledModule = ((Get-Module -Name Rubrik -ListAvailable) | ForEach-Object {if ($_.PrivateData.PSData.Prerelease -ne $null) {'{0}-{1}' -f $_.Version,$_.PrivateData.PSData.Prerelease} else {$_.version}}) -join ', '
+
+    # Add configured module options and default parameters
+    $HashProps.RubrikModuleOptions = Get-RubrikModuleOption
+    $HashProps.RubrikModuleDefaultParameters = Get-RubrikModuleDefaultParameter
 
     # Output as object
     return [pscustomobject]$HashProps
