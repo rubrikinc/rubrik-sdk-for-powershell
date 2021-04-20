6a6b6ac4f578baa27281128513ea38c7487dfb0a
Jake Robinson <jake.robinson@rubrik.com>
2021-04-20 13:37:03 -0400

    Fix one more area where convertfrom-json was trying to deserialize an object instead of .content

--- Tests/Submit-Request.Tests.ps1
+++ Tests/Submit-Request.Tests.ps1
@@ -47,10 +47,9 @@ Describe -Name 'Private/Submit-Request' -Tag 'Private', 'Submit-Request' -Fixtur
     
     $EventJson = [PSCustomObject]@{
         content = '{"hasMore":true,"data":[{"id":"2019-07-02:21:4:::1562103677178-d71cf9d8-c460-4411-8e34-c24a5d197824","objectId":"MssqlDatabase:::427e4ac6-7add-4240-9cde-e102b82a04b1","objectType":"Mssql","objectName":"ReportServer","eventInfo":"{\"message\":\"Replicated Microsoft SQL Server Database ''ReportServer'' from ''Cluster_A'' to ''Cluster_B''\",\"id\":\"Replication.ReplicationSucceeded\",\"params\":{\"${snappableType}\":\"Microsoft SQL Server Database\",\"${snappableName}\":\"ReportServer\",\"${sourceClusterName}\":\"Cluster_A\",\"${targetClusterName}\":\"Cluster_B\"}}","time":"Tue Jul 02 21:41:17 UTC 2019","eventType":"Replication","eventStatus":"Success","eventSeriesId":"ec203598-ea31-4929-ad76-c98744c3d489","relatedIds":"MssqlDatabase:::427e4ac6-7add-4240-9cde-e102b82a04b1","jobInstanceId":"PULL_REPLICATE_f6fe4db5-7f1a-4d51-a16c-0eef911dc033:::346"},{"id":"2019-07-02:21:4:::1562103732903-ca725080-210a-4577-b703-74c6ec6b9588","objectId":"CloudNativeSource:::02ff9580-adf2-464e-aeef-b77321b402fc","objectType":"AwsAccount","objectName":"AWS Matt","eventInfo":"{\"message\":\"Finished refreshing cloud native source AWS Matt (AWS).\",\"id\":\"CloudNative.RefreshCloudNativeSourceSucceeded\",\"params\":{\"${sourceName}\":\"AWS Matt\",\"${sourceType}\":\"AWS\"}}","time":"Tue Jul 02 21:42:12 UTC 2019","eventType":"CloudNativeSource","eventStatus":"Success","eventSeriesId":"6b09fa1a-0f21-41e2-bdb3-71bfac1b931b","relatedIds":"","jobInstanceId":"REFRESH_CLOUD_NATIVE_SOURCE_02ff9580-adf2-464e-aeef-b77321b402fc:::5268"}]}'
-
     }
     
-    $SLAObject = [pscustomobject]@{
+    $SLAObject = [PSCustomObject]@{
         id                            = '156eff81-b7d8-48c3-b3e5-97c2f788e215'
         primaryClusterId              = '8b4fe6f6-cc87-4354-a125-b65e23cf8c90'
         name                          = 'TestSLA_1 (Managed by Polaris)'
@@ -90,7 +89,6 @@ Describe -Name 'Private/Submit-Request' -Tag 'Private', 'Submit-Request' -Fixtur
         Mock -CommandName 'Invoke-RubrikWebRequest' -Verifiable -MockWith {
             [PSCustomObject]@{
                 statuscode = 204
-                Content = '{}'
             }
         }
 
@@ -123,7 +121,9 @@ Describe -Name 'Private/Submit-Request' -Tag 'Private', 'Submit-Request' -Fixtur
     
     Context -Name 'Test empty result filtering' {
         Mock -CommandName 'Invoke-RubrikWebRequest' -Verifiable -MockWith {
-            '{"statuscode": 204}'
+            [PSCustomObject]@{
+                statuscode = 204
+            }
         }
         
         $resources = @{
