4011eb8225d4dc1c1e92e2a7a33b8517946094a7
Mike Preston <mwpreston@gmail.com>
2022-01-04 09:29:47 -0500

    wrong test

--- Tests/Remove-RubrikFileset.Tests.ps1
+++ Tests/Remove-RubrikFileset.Tests.ps1
@@ -45,27 +45,4 @@ Describe -Name 'Public/Remove-RubrikFileset' -Tag 'Public', 'Remove-RubrikFilese
                 Should -Throw "Cannot bind argument to parameter 'id' because it is an empty string."
         }
     }
-
-    Context -Name 'Parameter/DoNotProtect/5.2' {
-        Mock -CommandName Test-RubrikConnection -Verifiable -ModuleName 'Rubrik' -MockWith {}
-        Mock -CommandName Test-RubrikSLA -Verifiable -ModuleName 'Rubrik' -MockWith {
-            'UNPROTECTED'
-        }
-        Mock -CommandName Submit-Request -Verifiable -ModuleName 'Rubrik' -MockWith {
-            @{ 
-                'id'                   = 'Fileset:::aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee-vm-12345'
-                'effectiveSlaDomainId' = 'UNPROTECTED'
-            }
-        }
-        It -Name 'Should use the 5.2 endpoint' -Test {
-            $rubrikconnection.version = '5.2.0'
-            $Output = & {
-                Protect-RubrikFileset -id 'Fileset::aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee-vm-12345' -DoNotProtect -Verbose 4>&1
-            }
-            (-join $Output) | Should -BeLike '*v2/sla_domain/UNPROTECTED/assign*'
-        }
-        Assert-VerifiableMock
-        Assert-MockCalled -CommandName Test-RubrikConnection -ModuleName 'Rubrik' -Times 1
-        Assert-MockCalled -CommandName Submit-Request -ModuleName 'Rubrik' -Times 1
-    }
 }
