18fe6057b89e8810ce742de8e6532c2a0391a156
Jaap Brasser <noreply@jaapbrasser.com>
2022-01-07 16:55:56 +0100

    Fixed DatabaseDownload tests 🤓

--- Tests/Get-RubrikDatabaseDownloadLink.Tests.ps1
+++ Tests/Get-RubrikDatabaseDownloadLink.Tests.ps1
@@ -15,20 +15,44 @@ Describe -Name 'Public/Get-RubrikDatabaseDownloadLink' -Tag 'Public', 'Get-Rubri
                 EventInfo = 'Download link for AdventureWorks'
                 Links = 'Thisshouldbefiltered***File_ValidateLink_Thisshouldbefiltered***'
                 Date = (Get-Date).AddHours(-1)
+                Time = (Get-Date).AddHours(+1)
             }
         }
+
         Mock -CommandName Test-RubrikConnection -Verifiable -ModuleName 'Rubrik' -MockWith {
             $Global:Header = @{
                 Authorization = 'Bearer test-authorization'
             }
         }
 
-        It -Name "Should return the correctly formatted download link based on input" -Test {
-            Get-RubrikDownloadLink -Server 'test-server' -SLAObject ([pscustomobject]@{sourceObjectType='test'}) -sourceDirs '\test' |
-                Should -Be 'https://test-server/download_dir/ASDFASDF'
+        Mock -CommandName Get-RubrikDatabase -Verifiable -ModuleName 'Rubrik' -MockWith {
+            $output = @{
+                Authorization = 'Bearer test-authorization'
+            }
         }
 
-        Assert-VerifiableMock
+        Mock -CommandName Get-RubrikSnapshot -Verifiable -ModuleName 'Rubrik' -MockWith {
+            @{
+                id = 'Bearer test-authorization'
+            }
+        }
+
+        $global:rubrikConnection = @{
+            id      = 'test-id'
+            userId  = 'test-userId'
+            token   = 'test-token'
+            server  = 'test-server'
+            header  = @{ 'Authorization' = 'Bearer test-authorization' }
+            time    = (Get-Date)
+            api     = 'v1'
+            version = '4.0.5'
+        }  
+
+        It -Name "Should return the correctly formatted download link based on input" -Test {
+            Get-RubrikDatabaseDownloadLink -Server 'test-server' -id 'id' -SnapshotId "Latest" |
+                Should -Be 'https://test-server/Download link for AdventureWorks'
+        }
+
         Assert-MockCalled -CommandName Test-RubrikConnection -ModuleName 'Rubrik' -Exactly 1
         Assert-MockCalled -CommandName Submit-Request -ModuleName 'Rubrik' -Exactly 1
         Assert-MockCalled -CommandName Get-RubrikEvent -ModuleName 'Rubrik' -Exactly 1
