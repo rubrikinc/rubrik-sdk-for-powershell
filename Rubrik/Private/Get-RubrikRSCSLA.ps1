06cd613a1b831fb2d691e0ec58244b39e7d4ffe7
Mike Preston <mwpreston@gmail.com>
2024-02-12 16:11:48 -0500

    updated get-RubrikRSCSla

--- Rubrik/Private/Get-RubrikRSCSLA.ps1
+++ Rubrik/Private/Get-RubrikRSCSLA.ps1
@@ -32,18 +32,8 @@ function Get-RubrikRSCSLA {
       [String]$api = $global:RubrikConnection.api
     )
 
-    if ($PSBoundParameters['Name']) {
-      $filter = @{
-          "field" = "NAME"
-          "text" = "$($Name)"
-      }
-      $query = New-RSCQuery -GqlQuery slaDomains
-      $query.var.filter = $filter
-      $response = (Invoke-Rsc $query).nodes
-      # since there is no exact match
-      Write-Verbose -Message "Matching $Name exactly"
-      $response = $response | where {$_.name -eq "$Name"}
-    } elseif ($PSBoundParameters['Id']) {
+
+    if ($PSBoundParameters['Id']) {
       <#
       # The following code is preferred, however is not currently working due to a bug in the SDK with the slaDomain GQL query implementing Cluster and Global SLAs
       # Will comment out for now and use the original way of handcrafting the GQL queries...
@@ -58,9 +48,67 @@ function Get-RubrikRSCSLA {
       $response = $response.data.q
     }
     else {
-      $query = New-RscQuery -GqlQuery slaDomains
-      $response = (Invoke-Rsc $query).nodes
+      $query = New-RSCQuery -GqlQuery slaDomains
+      # Add Fields
+      # These are currently not working as they are getting added to ClusterSlaDomain instead of GlobalSlaReply
+      # Shared Types to be reused
+      $Duration = New-Object -TypeName RubrikSecurityCloud.Types.Duration
+      $Duration.Unit = "HOURS"
+      $Duration.DurationField = 24
+      $Schedule = New-Object -TypeName RubrikSecurityCloud.Types.BasicSnapshotSchedule
+      $Schedule.Retention = 1
+      $Schedule.Frequency = 1
+      $Schedule.RetentionUnit = "DAYS"
+      # ProtectedObjectCount
+      $query.field.nodes[0].ProtectedObjectCount = 0
+      # Base Frequency
+      $query.field.nodes[0].BaseFrequency = $Duration
+      # Snapshot Schedule
+      $query.field.Nodes[0].SnapshotSchedule = New-Object -TypeName RubrikSecurityCloud.types.SnapshotSchedule
+      $query.field.nodes[0].SnapshotSchedule.minute = New-Object -TypeName RubrikSecurityCloud.types.minuteSnapshotSchedule
+      $query.field.nodes[0].SnapshotSchedule.minute.BasicSchedule = $Schedule
+      $query.field.nodes[0].SnapshotSchedule.hourly = New-Object -TypeName RubrikSecurityCloud.types.hourlySnapshotSchedule
+      $query.field.nodes[0].SnapshotSchedule.hourly.BasicSchedule = $Schedule
+      $query.field.nodes[0].SnapshotSchedule.daily = New-Object -TypeName RubrikSecurityCloud.types.dailySnapshotSchedule
+      $query.field.nodes[0].SnapshotSchedule.daily.BasicSchedule = $Schedule
+      $query.field.nodes[0].SnapshotSchedule.weekly = New-Object -TypeName RubrikSecurityCloud.types.WeeklySnapshotSchedule
+      $query.field.nodes[0].SnapshotSchedule.weekly.BasicSchedule = $Schedule
+      $query.field.nodes[0].SnapshotSchedule.weekly.DayOfWeek = "SUNDAY"
+      $query.field.nodes[0].SnapshotSchedule.monthly = New-Object -TypeName RubrikSecurityCloud.types.monthlySnapshotSchedule
+      $query.field.nodes[0].SnapshotSchedule.monthly.BasicSchedule = $Schedule
+      $query.field.nodes[0].SnapshotSchedule.monthly.DayOfMonth = "FIRST_DAY"
+      $query.field.nodes[0].SnapshotSchedule.quarterly = New-Object -TypeName RubrikSecurityCloud.types.quarterlySnapshotSchedule
+      $query.field.nodes[0].SnapshotSchedule.quarterly.BasicSchedule = $Schedule
+      $query.field.nodes[0].SnapshotSchedule.quarterly.dayOfQuarter = "FIRST_DAY"
+      $query.field.nodes[0].SnapshotSchedule.quarterly.quarterStartMonth = "JANUARY"
+      $query.field.nodes[0].SnapshotSchedule.yearly = New-Object -TypeName RubrikSecurityCloud.types.YearlySnapshotSchedule
+      $query.field.nodes[0].SnapshotSchedule.yearly.BasicSchedule = $Schedule
+      $query.field.nodes[0].SnapshotSchedule.yearly.dayOfYear = "FIRST_DAY"
+      $query.field.nodes[0].SnapshotSchedule.yearly.yearStartMonth = "JANUARY"
+      # Local Retention 
+      $query.field.nodes[0].localRetentionLimit = $Duration
+      
+      if ($PSBoundParameters['Name']) {
+        $filter = @{
+            "field" = "NAME"
+            "text" = "$($Name)"
+        }
+        $query.var.filter = $filter
+        $response = (Invoke-Rsc $query).nodes
+        # since there is no exact match
+        Write-Verbose -Message "Matching $Name exactly"
+        $response = $response | where {$_.name -eq "$Name"}
+      } else {
+        $query = New-RscQuery -GqlQuery slaDomains
+        $response = (Invoke-Rsc $query).nodes
+      }
+
     }
 
+  $response = $response | Select-Object -Property *, @{
+    Name="isRetentionLocked"; Expression={$_.isRetentionLockedSla}
+  },@{
+    Name="numProtectedObjects"; Expression={$_.ProtectedObjectCount}
+  }
   return $response
 }
