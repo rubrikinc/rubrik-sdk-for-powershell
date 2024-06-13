e092a285b7667707bcbc23b522b5d2dfbf8cce07
Mike Preston <mwpreston@gmail.com>
2024-06-13 14:01:34 -0400

    -force RSC Module load

--- azure-pipelines/scripts/Invoke-RunTests.ps1
+++ azure-pipelines/scripts/Invoke-RunTests.ps1
@@ -1,7 +1,7 @@
 Install-Module -Name Pester -MaximumVersion 4.2.0 -Force
 Remove-Module Pester -ErrorAction SilentlyContinue
 Import-Module -Name Pester -MaximumVersion 4.2.0 
-Install-Module -Name RubrikSecurityCloud
+Install-Module -Name RubrikSecurityCloud -Force
 Import-Module RubrikSecurityCloud
 
 $PesterSplat = @{
